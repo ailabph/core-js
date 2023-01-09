c7b00eff125a714fe55bf494a780fd60c180c5be
adminailab <96946812+adminailab@users.noreply.github.com>
2023-01-09 18:12:13 +0800

    remove ds file

--- mysql2-promse.d.ts
+++
@@ -1,6 +0,0 @@
-
-declare module "mysql2-promise"{
-    import {Connection, Pool, PoolConnection} from "mysql2/promise";
-    import * as mysql from "mysql2-promise";
-    export { Connection, Pool, PoolConnection };
-}
