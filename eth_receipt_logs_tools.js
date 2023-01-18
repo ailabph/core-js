7856b207fe5429a68fe5286163d9a9f78c915997
adminailab <96946812+adminailab@users.noreply.github.com>
2023-01-18 22:30:30 +0800

    class to retrieve receipt/update & logs from db

---
+++ eth_receipt_logs_tools.js
@@ -0,0 +1,149 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.eth_receipt_logs_tools = void 0;
+const assert_1 = require("./assert");
+const eth_receipt_1 = require("./build/eth_receipt");
+const eth_worker_1 = require("./eth_worker");
+const eth_receipt_logs_1 = require("./build/eth_receipt_logs");
+const eth_log_decoder_1 = require("./eth_log_decoder");
+class eth_receipt_logs_tools {
+    static getReceiptLogs(txn_hash) {
+        var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m, _o, _p, _q, _r, _s, _t, _u, _v;
+        return __awaiter(this, void 0, void 0, function* () {
+            assert_1.assert.inTransaction();
+            assert_1.assert.notEmpty(txn_hash, "txn_hash");
+            let receipt_db = new eth_receipt_1.eth_receipt();
+            receipt_db.transactionHash = txn_hash;
+            yield receipt_db.fetch();
+            if (receipt_db.isNew()) {
+                console.log(`hash:${txn_hash} receipt not on db, retrieving in rpc...`);
+                const receipt = yield eth_worker_1.eth_worker.getReceiptByTxnHashWeb3(txn_hash);
+                receipt_db.blockHash = receipt.blockHash;
+                receipt_db.blockNumber = receipt.blockNumber;
+                receipt_db.contractAddress = (_a = receipt.contractAddress) !== null && _a !== void 0 ? _a : "";
+                receipt_db.cumulativeGasUsed = receipt.cumulativeGasUsed.toString();
+                receipt_db.fromAddress = receipt.from;
+                receipt_db.gasUsed = receipt.gasUsed.toString();
+                receipt_db.logsBloom = receipt.logsBloom;
+                receipt_db.status = receipt.status ? "true" : "false";
+                receipt_db.toAddress = receipt.to;
+                receipt_db.transactionHash = receipt.transactionHash;
+                receipt_db.transactionIndex = receipt.transactionIndex;
+                yield receipt_db.save();
+                let logCheck = new eth_receipt_logs_1.eth_receipt_logs();
+                yield logCheck.list(" WHERE receipt_id=:receipt_id ", { receipt_id: receipt_db.id });
+                if (logCheck.count() > 0)
+                    throw new Error(`logs already set for receipt id:${receipt_db.id} txn_hash:${receipt_db.transactionHash}`);
+                for (let i = 0; i < receipt.logs.length; i++) {
+                    const log = receipt.logs[i];
+                    let logs_db = new eth_receipt_logs_1.eth_receipt_logs();
+                    logs_db.receipt_id = receipt_db.id;
+                    logs_db.txn_hash = log.transactionHash;
+                    logs_db.address = log.address;
+                    logs_db.topics = JSON.stringify(log.topics);
+                    logs_db.data = log.data;
+                    logs_db.blockNumber = log.blockNumber;
+                    logs_db.transactionHash = log.transactionHash;
+                    logs_db.transactionIndex = log.transactionIndex;
+                    logs_db.blockHash = log.blockHash;
+                    logs_db.logIndex = log.logIndex;
+                    yield logs_db.save();
+                }
+                return eth_receipt_logs_tools.getReceiptLogs(txn_hash);
+            }
+            else {
+                let receipt = {
+                    blockHash: (_b = receipt_db.blockHash) !== null && _b !== void 0 ? _b : "",
+                    blockNumber: (_c = receipt_db.blockNumber) !== null && _c !== void 0 ? _c : 0,
+                    contractAddress: (_d = receipt_db.contractAddress) !== null && _d !== void 0 ? _d : "",
+                    cumulativeGasUsed: parseFloat((_e = receipt_db.cumulativeGasUsed) !== null && _e !== void 0 ? _e : "0"),
+                    events: {},
+                    from: (_f = receipt_db.fromAddress) !== null && _f !== void 0 ? _f : "",
+                    gasUsed: parseFloat((_g = receipt_db.gasUsed) !== null && _g !== void 0 ? _g : "0"),
+                    logs: [],
+                    logsBloom: (_h = receipt_db.logsBloom) !== null && _h !== void 0 ? _h : "",
+                    status: ((_j = receipt_db.status) !== null && _j !== void 0 ? _j : "false") === "true",
+                    to: (_k = receipt_db.toAddress) !== null && _k !== void 0 ? _k : "",
+                    transactionHash: (_l = receipt_db.transactionHash) !== null && _l !== void 0 ? _l : "",
+                    transactionIndex: (_m = receipt_db.transactionIndex) !== null && _m !== void 0 ? _m : 0
+                };
+                let logs = new eth_receipt_logs_1.eth_receipt_logs();
+                yield logs.list(" WHERE receipt_id=:receipt_id ", { receipt_id: receipt_db.id });
+                let log = new eth_receipt_logs_1.eth_receipt_logs();
+                while (log = logs.getItem()) {
+                    let topic_log = {
+                        address: (_o = log.address) !== null && _o !== void 0 ? _o : "",
+                        blockHash: (_p = log.blockHash) !== null && _p !== void 0 ? _p : "",
+                        blockNumber: (_q = log.blockNumber) !== null && _q !== void 0 ? _q : 0,
+                        data: (_r = log.data) !== null && _r !== void 0 ? _r : "",
+                        logIndex: (_s = log.logIndex) !== null && _s !== void 0 ? _s : 0,
+                        topics: JSON.parse((_t = log.topics) !== null && _t !== void 0 ? _t : "[]"),
+                        transactionHash: (_u = log.transactionHash) !== null && _u !== void 0 ? _u : "",
+                        transactionIndex: (_v = log.transactionIndex) !== null && _v !== void 0 ? _v : 0
+                    };
+                    receipt.logs.push(topic_log);
+                }
+                return { receipt: receipt, result: [] };
+            }
+        });
+    }
+    static getFirstTopicLog(txn_hash) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const analyzeLogsResult = typeof txn_hash === "string" ? yield eth_receipt_logs_tools.getReceiptLogs(txn_hash) : txn_hash;
+            if (analyzeLogsResult.receipt.logs.length === 0)
+                throw new Error(`transaction(${txn_hash}) has no log topics`);
+            return yield eth_log_decoder_1.eth_log_decoder.decodeLog(analyzeLogsResult.receipt.logs[0]);
+        });
+    }
+    static getLastTopicLog(txn_hash) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const analyzeLogsResult = typeof txn_hash === "string" ? yield eth_receipt_logs_tools.getReceiptLogs(txn_hash) : txn_hash;
+            if (analyzeLogsResult.receipt.logs.length === 0)
+                throw new Error(`transaction(${txn_hash}) has no log topics`);
+            return yield eth_log_decoder_1.eth_log_decoder.decodeLog(analyzeLogsResult.receipt.logs[analyzeLogsResult.receipt.logs.length - 1]);
+        });
+    }
+    static getFirstLogByMethod(txn_hash, method_name, strict = false) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const analyzeLogsResult = typeof txn_hash === "string" ? yield eth_receipt_logs_tools.getReceiptLogs(txn_hash) : txn_hash;
+            if (analyzeLogsResult.receipt.logs.length === 0)
+                throw new Error(`transaction(${txn_hash}) has no log topics`);
+            for (const log of analyzeLogsResult.receipt.logs) {
+                const decodedLog = yield eth_log_decoder_1.eth_log_decoder.decodeLog(log);
+                if (decodedLog.method_name.toLowerCase() === method_name.toLowerCase()) {
+                    return decodedLog;
+                }
+            }
+            if (strict)
+                throw new Error(`hash:${analyzeLogsResult.receipt.transactionHash} unable to find log:${method_name}`);
+            return false;
+        });
+    }
+    static getLastLogByMethod(txn_hash, method_name, strict = false) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const analyzeLogsResult = typeof txn_hash === "string" ? yield eth_receipt_logs_tools.getReceiptLogs(txn_hash) : txn_hash;
+            if (analyzeLogsResult.receipt.logs.length === 0)
+                throw new Error(`transaction(${txn_hash}) has no log topics`);
+            let to_return = false;
+            for (const log of analyzeLogsResult.receipt.logs) {
+                const decodedLog = yield eth_log_decoder_1.eth_log_decoder.decodeLog(log);
+                if (decodedLog.method_name.toLowerCase() === method_name.toLowerCase()) {
+                    to_return = decodedLog;
+                }
+            }
+            if (!to_return && strict)
+                throw new Error(`hash:${analyzeLogsResult.receipt.transactionHash} unable to find log:${method_name}`);
+            return to_return ? to_return : false;
+        });
+    }
+}
+exports.eth_receipt_logs_tools = eth_receipt_logs_tools;
---
+++ eth_receipt_logs_tools.ts
@@ -0,0 +1,131 @@
+import {TransactionReceipt} from "web3-eth/types";
+import {AnalyzeLogsResult} from "./eth_types";
+import {assert} from "./assert";
+import {eth_receipt} from "./build/eth_receipt";
+import {eth_worker} from "./eth_worker";
+import {eth_receipt_logs} from "./build/eth_receipt_logs";
+import {Log} from "web3-core";
+import {BaseType, eth_log_decoder} from "./eth_log_decoder";
+
+export class eth_receipt_logs_tools{
+
+    public static async getReceiptLogs(txn_hash:string): Promise<AnalyzeLogsResult>{
+        assert.inTransaction();
+        assert.notEmpty(txn_hash,"txn_hash");
+        let receipt_db = new eth_receipt();
+        receipt_db.transactionHash = txn_hash;
+        await receipt_db.fetch();
+        if(receipt_db.isNew()){
+            console.log(`hash:${txn_hash} receipt not on db, retrieving in rpc...`)
+            const receipt = await eth_worker.getReceiptByTxnHashWeb3(txn_hash);
+            receipt_db.blockHash = receipt.blockHash;
+            receipt_db.blockNumber = receipt.blockNumber;
+            receipt_db.contractAddress = receipt.contractAddress ?? "";
+            receipt_db.cumulativeGasUsed = receipt.cumulativeGasUsed.toString();
+            receipt_db.fromAddress = receipt.from;
+            receipt_db.gasUsed = receipt.gasUsed.toString();
+            receipt_db.logsBloom = receipt.logsBloom;
+            receipt_db.status = receipt.status ? "true" : "false";
+            receipt_db.toAddress = receipt.to;
+            receipt_db.transactionHash = receipt.transactionHash;
+            receipt_db.transactionIndex = receipt.transactionIndex;
+            await receipt_db.save();
+
+            let logCheck = new eth_receipt_logs();
+            await logCheck.list(" WHERE receipt_id=:receipt_id ",{receipt_id:receipt_db.id});
+            if(logCheck.count() > 0) throw new Error(`logs already set for receipt id:${receipt_db.id} txn_hash:${receipt_db.transactionHash}`);
+
+            for(let i=0; i < receipt.logs.length; i++){
+                const log = receipt.logs[i];
+                let logs_db = new eth_receipt_logs();
+                logs_db.receipt_id = receipt_db.id;
+                logs_db.txn_hash = log.transactionHash;
+                logs_db.address = log.address;
+                logs_db.topics = JSON.stringify(log.topics);
+                logs_db.data = log.data;
+                logs_db.blockNumber = log.blockNumber;
+                logs_db.transactionHash = log.transactionHash;
+                logs_db.transactionIndex = log.transactionIndex;
+                logs_db.blockHash = log.blockHash;
+                logs_db.logIndex = log.logIndex;
+                await logs_db.save();
+            }
+
+            return eth_receipt_logs_tools.getReceiptLogs(txn_hash);
+        }
+        else{
+            let receipt: TransactionReceipt = {
+                blockHash: receipt_db.blockHash ?? "",
+                blockNumber: receipt_db.blockNumber ?? 0,
+                contractAddress: receipt_db.contractAddress ?? "",
+                cumulativeGasUsed: parseFloat(receipt_db.cumulativeGasUsed ?? "0"),
+                events: {},
+                from: receipt_db.fromAddress ?? "",
+                gasUsed: parseFloat(receipt_db.gasUsed ?? "0"),
+                logs: [],
+                logsBloom: receipt_db.logsBloom ?? "",
+                status: (receipt_db.status ?? "false") === "true",
+                to: receipt_db.toAddress ?? "",
+                transactionHash: receipt_db.transactionHash ?? "",
+                transactionIndex: receipt_db.transactionIndex ?? 0
+            };
+            let logs = new eth_receipt_logs();
+            await logs.list(" WHERE receipt_id=:receipt_id ",{receipt_id:receipt_db.id});
+            let log = new eth_receipt_logs();
+            while(log = logs.getItem()){
+                let topic_log:Log = {
+                    address: log.address ?? "",
+                    blockHash: log.blockHash ?? "",
+                    blockNumber: log.blockNumber ?? 0,
+                    data: log.data ?? "",
+                    logIndex: log.logIndex ?? 0,
+                    topics: JSON.parse(log.topics ?? "[]"),
+                    transactionHash: log.transactionHash ?? "",
+                    transactionIndex: log.transactionIndex ?? 0
+                };
+                receipt.logs.push(topic_log);
+            }
+            return {receipt: receipt, result: []};
+        }
+    }
+
+    public static async getFirstTopicLog(txn_hash:string|AnalyzeLogsResult): Promise<BaseType>{
+        const analyzeLogsResult = typeof txn_hash === "string" ? await eth_receipt_logs_tools.getReceiptLogs(txn_hash) : txn_hash;
+        if(analyzeLogsResult.receipt.logs.length === 0) throw new Error(`transaction(${txn_hash}) has no log topics`);
+        return await eth_log_decoder.decodeLog(analyzeLogsResult.receipt.logs[0]);
+    }
+
+    public static async getLastTopicLog(txn_hash:string|AnalyzeLogsResult): Promise<BaseType|false>{
+        const analyzeLogsResult = typeof txn_hash === "string" ? await eth_receipt_logs_tools.getReceiptLogs(txn_hash) : txn_hash;
+        if(analyzeLogsResult.receipt.logs.length === 0) throw new Error(`transaction(${txn_hash}) has no log topics`);
+        return await eth_log_decoder.decodeLog(analyzeLogsResult.receipt.logs[analyzeLogsResult.receipt.logs.length-1]);
+    }
+
+    public static async getFirstLogByMethod<T>(txn_hash:string|AnalyzeLogsResult,method_name:string,strict:boolean = false): Promise<T|false>{
+        const analyzeLogsResult = typeof txn_hash === "string" ? await eth_receipt_logs_tools.getReceiptLogs(txn_hash) : txn_hash;
+        if(analyzeLogsResult.receipt.logs.length === 0) throw new Error(`transaction(${txn_hash}) has no log topics`);
+        for(const log of analyzeLogsResult.receipt.logs){
+            const decodedLog = await eth_log_decoder.decodeLog(log);
+            if(decodedLog.method_name.toLowerCase() === method_name.toLowerCase()){
+                return decodedLog as unknown as T;
+            }
+        }
+        if(strict) throw new Error(`hash:${analyzeLogsResult.receipt.transactionHash} unable to find log:${method_name}`);
+        return false;
+    }
+
+    public static async getLastLogByMethod<T>(txn_hash:string|AnalyzeLogsResult,method_name:string,strict:boolean = false): Promise<T|false>{
+        const analyzeLogsResult = typeof txn_hash === "string" ? await eth_receipt_logs_tools.getReceiptLogs(txn_hash) : txn_hash;
+        if(analyzeLogsResult.receipt.logs.length === 0) throw new Error(`transaction(${txn_hash}) has no log topics`);
+        let to_return:T|boolean = false;
+        for(const log of analyzeLogsResult.receipt.logs){
+            const decodedLog = await eth_log_decoder.decodeLog(log);
+            if(decodedLog.method_name.toLowerCase() === method_name.toLowerCase()){
+                to_return = decodedLog as unknown as T;
+            }
+        }
+        if(!to_return && strict) throw new Error(`hash:${analyzeLogsResult.receipt.transactionHash} unable to find log:${method_name}`);
+        return to_return ? to_return : false;
+    }
+
+}
