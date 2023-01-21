4e323993d6edf240bceda8ab80e9c88531a24bcd
adminailab <96946812+adminailab@users.noreply.github.com>
2023-01-21 21:03:36 +0800

    new class for rpc, provider initializer and getter

---
+++ eth_rpc.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.eth_rpc = void 0;
+const web3_1 = __importDefault(require("web3"));
+const eth_config_1 = require("./eth_config");
+class eth_rpc {
+    static getWeb3Provider() {
+        if (typeof eth_rpc.web3Provider !== "undefined")
+            return eth_rpc.web3Provider;
+        eth_rpc.web3Provider = new web3_1.default.providers.HttpProvider(eth_config_1.eth_config.getRPCUrl());
+        return eth_rpc.web3Provider;
+    }
+    static getWeb3Client() {
+        if (typeof eth_rpc.web3Client !== "undefined")
+            return eth_rpc.web3Client;
+        eth_rpc.web3Client = new web3_1.default(eth_rpc.getWeb3Provider());
+        return eth_rpc.web3Client;
+    }
+}
+exports.eth_rpc = eth_rpc;
---
+++ eth_rpc.ts
@@ -0,0 +1,20 @@
+import Web3 from "web3";
+
+import { HttpProvider } from "web3-core";
+import {eth_config} from "./eth_config";
+export class eth_rpc{
+    private static web3Provider:HttpProvider|undefined;
+    private static web3Client:Web3|undefined;
+
+    public static getWeb3Provider():HttpProvider{
+        if(typeof eth_rpc.web3Provider !== "undefined") return eth_rpc.web3Provider;
+        eth_rpc.web3Provider = new Web3.providers.HttpProvider(eth_config.getRPCUrl());
+        return eth_rpc.web3Provider;
+    }
+
+    public static getWeb3Client():Web3{
+        if(typeof eth_rpc.web3Client !== "undefined") return eth_rpc.web3Client;
+        eth_rpc.web3Client = new Web3(eth_rpc.getWeb3Provider());
+        return eth_rpc.web3Client;
+    }
+}
