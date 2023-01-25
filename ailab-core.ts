6e0a0aa8c9976032261ee3978f220e1aabe24f34
adminailab <96946812+adminailab@users.noreply.github.com>
2023-01-25 21:39:35 +0800

    export all classes to main

--- ailab-core.ts
+++ ailab-core.ts
@@ -1,7 +1,151 @@
-import {build} from "./build";
-import {config} from "./config";
-import {tools} from "./tools";
-import {connection} from "./connection";
-import {dataObject} from "./dataObject";
+import { assert} from "./assert";
+export { assert };
 
-export { build, config, tools, connection, dataObject };
+import { assert_eth } from "./assert_eth";
+export { assert_eth };
+
+import { build } from "./build";
+export { build };
+
+import { config } from "./config";
+export { config };
+
+import { connection } from "./connection";
+export { connection };
+
+import { dataObject } from "./dataObject";
+export { dataObject };
+
+import {
+    eth_abi_decoder,
+    DecodedMethodArguments,
+    DecodedAbi,
+    DecodedAbiObject,
+    transfer,
+    approve,
+    addLiquidityETH,
+    swapETHForExactTokens,
+    swapExactETHForTokens,
+    excludeFromFee,
+    swapExactTokensForTokens,
+    setNumTokensSellToAddToLiquidity,
+    clearStuckBNBBalance,
+    swapTokensForExactETH,
+    setMarketFee,
+    setSellFeeMultiplier,
+    setLiquidityFee,
+    swapExactTokensForETHSupportingFeeOnTransferTokens,
+    swapExactETHForTokensSupportingFeeOnTransferTokens,
+    swapExactTokensForTokensSupportingFeeOnTransferTokens,
+    swap,
+} from "./eth_abi_decoder";
+export {
+    eth_abi_decoder,
+    DecodedMethodArguments,
+    DecodedAbi,
+    DecodedAbiObject,
+    transfer,
+    approve,
+    addLiquidityETH,
+    swapETHForExactTokens,
+    swapExactETHForTokens,
+    excludeFromFee,
+    swapExactTokensForTokens,
+    setNumTokensSellToAddToLiquidity,
+    clearStuckBNBBalance,
+    swapTokensForExactETH,
+    setMarketFee,
+    setSellFeeMultiplier,
+    setLiquidityFee,
+    swapExactTokensForETHSupportingFeeOnTransferTokens,
+    swapExactETHForTokensSupportingFeeOnTransferTokens,
+    swapExactTokensForTokensSupportingFeeOnTransferTokens,
+    swap,
+};
+
+import { eth_config } from "./eth_config";
+export { eth_config };
+
+import {
+    eth_log_decoder,
+    BaseType,
+    TransferLog,
+    SwapLog,
+    ApprovalLog,
+    WithdrawalLog,
+    SyncLog,
+    DepositLog,
+    MintLog,
+} from "./eth_log_decoder";
+export {
+    eth_log_decoder,
+    BaseType,
+    TransferLog,
+    SwapLog,
+    ApprovalLog,
+    WithdrawalLog,
+    SyncLog,
+    DepositLog,
+    MintLog,
+};
+
+import { eth_receipt_logs_tools } from "./eth_receipt_logs_tools";
+export { eth_receipt_logs_tools };
+
+import { eth_rpc } from "./eth_rpc";
+export { eth_rpc };
+
+import { eth_tools } from "./eth_tools";
+export { eth_tools };
+
+import { eth_transaction_tools } from "./eth_transaction_tools";
+export { eth_transaction_tools };
+
+import {
+    AnalysisResult,
+    AnalyzeLogsResult, BnbUsdReserve,
+    ContractInfo,
+    eth_types,
+    GasInfo,
+    LogData,
+    LogSigArgs,
+    RESULT_SEND_STATUS,
+    RESULT_STATUS, TokenBnbReserve,
+    WalletInfo
+} from "./eth_types";
+export {
+    AnalysisResult,
+    AnalyzeLogsResult, BnbUsdReserve,
+    ContractInfo,
+    eth_types,
+    GasInfo,
+    LogData,
+    LogSigArgs,
+    RESULT_SEND_STATUS,
+    RESULT_STATUS, TokenBnbReserve,
+    WalletInfo
+};
+
+import { eth_worker } from "./eth_worker";
+export { eth_worker };
+
+import { eth_worker_block } from "./eth_worker_block";
+export { eth_worker_block };
+
+import { eth_worker_events } from "./eth_worker_events";
+export { eth_worker_events };
+
+import { eth_worker_trade } from "./eth_worker_trade";
+export { eth_worker_trade };
+
+import { eth_worker_trade_strategy } from "./eth_worker_trade_strategy";
+export { eth_worker_trade_strategy };
+
+import { eth_worker_txn } from "./eth_worker_txn";
+export { eth_worker_txn };
+
+import { logger } from "./logger";
+export { logger };
+
+import { tools } from "./tools";
+export { tools };
