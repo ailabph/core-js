73ef914d00e7debbdc8902c01885e64bc53f458f
adminailab <96946812+adminailab@users.noreply.github.com>
2023-01-25 21:41:36 +0800

    import via ailab-core

---
+++ eth_worker_trade.js
@@ -0,0 +1,30 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.eth_worker_trade = void 0;
+class eth_worker_trade {
+    static swap(srcToken, destToken) {
+        return __awaiter(this, void 0, void 0, function* () { });
+    }
+    static swapBuy() {
+        return __awaiter(this, void 0, void 0, function* () { });
+    }
+    static swapSell() {
+        return __awaiter(this, void 0, void 0, function* () { });
+    }
+    static getTokenBalance() {
+        return __awaiter(this, void 0, void 0, function* () { });
+    }
+    static getBnbBalance() {
+        return __awaiter(this, void 0, void 0, function* () { });
+    }
+}
+exports.eth_worker_trade = eth_worker_trade;
---
+++ eth_worker_trade.ts
@@ -0,0 +1,10 @@
+
+export class eth_worker_trade{
+
+    public static async swap(srcToken:string,destToken:string,){}
+    public static async swapBuy(){}
+    public static async swapSell(){}
+    public static async getTokenBalance(){}
+    public static async getBnbBalance(){}
+
+}
