538d2741b75f0fbdaba6235158edeab7a8b8e6ca
adminailab <96946812+adminailab@users.noreply.github.com>
2023-02-22 13:34:25 +0800

    refactor class name

---
+++ eth_price_track_details_tools.js
@@ -0,0 +1,199 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.eth_price_track_details_tools = void 0;
+const eth_price_track_details_1 = require("./build/eth_price_track_details");
+const assert_1 = require("./assert");
+const config_1 = require("./config");
+const eth_config_1 = require("./eth_config");
+const tools_1 = require("./tools");
+const eth_price_track_header_tools_1 = require("./eth_price_track_header_tools");
+const time_helper_1 = require("./time_helper");
+const eth_contract_data_tools_1 = require("./eth_contract_data_tools");
+var ORDER;
+(function (ORDER) {
+    ORDER["ASC"] = "ASC";
+    ORDER["DESC"] = "DESC";
+})(ORDER || (ORDER = {}));
+class eth_price_track_details_tools {
+    static log(msg, method, end = false, force_display = false) {
+        if (config_1.config.getConfig().verbose_log || force_display) {
+            console.log(`${this.name}|${method}|${msg}`);
+            if (end)
+                console.log(tools_1.tools.LINE);
+        }
+    }
+    static async getDetails(header, { from_time = 0, to_time = 0, log = undefined, order = ORDER.DESC, limit = 1 }, strict = false) {
+        const method = `getDetails`;
+        this.log(`retrieving price details. establishing header`, method);
+        const details = new eth_price_track_details_1.eth_price_track_details();
+        try {
+            const retrievedHeader = await eth_price_track_header_tools_1.eth_price_track_header_tools.getViaIdOrContractStrict(header);
+            const orderString = ` ORDER BY blockNumber ${order}, logIndex ${order} LIMIT ${limit} `;
+            if (log) {
+                this.log(`db log passed, retrieving via blockNumber(${log.blockNumber}) and logIndex(${log.logIndex})`, method);
+                await details.list(" WHERE header_id=:header_id AND blockNumber=:blockNumber AND logIndex<=:logIndex ", { header_id: retrievedHeader.id, blockNumber: assert_1.assert.positiveNumber(log.blockNumber), logIndex: assert_1.assert.naturalNumber(log.logIndex) }, orderString);
+            }
+            if (details.count() === 0) {
+                if (log) {
+                    this.log(`no details found via logs method, retrieving via timestamp`, method);
+                }
+                let where = " WHERE header_id=:header_id ";
+                let param = {};
+                param["header_id"] = assert_1.assert.positiveNumber(retrievedHeader.id);
+                if (from_time > 0) {
+                    const fromTimeInfo = time_helper_1.time_helper.getTime(from_time, "UTC");
+                    this.log(`from ${fromTimeInfo.format(time_helper_1.TIME_FORMATS.READABLE)}`, method);
+                    where += " AND blockTime>=:from ";
+                    param["from"] = from_time;
+                }
+                else {
+                    this.log(`no from_time info`, method);
+                }
+                if (to_time > 0) {
+                    const toTimeInfo = time_helper_1.time_helper.getTime(to_time, "UTC");
+                    this.log(`to ${toTimeInfo.format(time_helper_1.TIME_FORMATS.READABLE)}`, method);
+                    where += " AND blockTime<=:to ";
+                    param["to"] = to_time;
+                }
+                await details.list(where, param, orderString);
+            }
+        }
+        catch (e) {
+            if (e instanceof Error) {
+                this.log(e.message, method, true, true);
+                if (strict)
+                    throw new eth_price_track_details_tools_error(e.message);
+            }
+            return false;
+        }
+        this.log(`found:${details.count()}`, method, true);
+        return details;
+    }
+    static async getDetail(header, time_or_log, strict = false) {
+        const method = "getDetail";
+        let detail = new eth_price_track_details_1.eth_price_track_details();
+        try {
+            header = await eth_price_track_header_tools_1.eth_price_track_header_tools.getViaIdOrContractStrict(header);
+            this.log(`retrieving price for ${eth_price_track_header_tools_1.eth_price_track_header_tools.getOrderedPairSymbol(header)}`, method);
+            const arg = {};
+            arg["order"] = ORDER.DESC;
+            arg["limit"] = 1;
+            if (typeof time_or_log === "number" || typeof time_or_log === "string") {
+                this.log(`time argument passed:${time_or_log}`, method);
+                const timeInfo = time_helper_1.time_helper.getTime(time_or_log, "UTC");
+                arg["to_time"] = timeInfo.unix();
+            }
+            else {
+                this.log(`db log argument passed`, method);
+                arg["log"] = time_or_log;
+            }
+            const details = await this.getDetails(header, arg, strict);
+            if (!details)
+                throw new Error(`no price details found`);
+            if (details.count() === 0)
+                throw new Error(`no price details found`);
+            detail = details.getItem();
+        }
+        catch (e) {
+            if (e instanceof Error) {
+                this.log(e.message, method, true, true);
+                if (strict)
+                    throw new eth_price_track_details_tools_error(e.message);
+            }
+        }
+        return detail;
+    }
+    static async getPrice(header, time_or_log, strict = false) {
+        const method = "getPrice";
+        let price = "0.00";
+        try {
+            header = await eth_price_track_header_tools_1.eth_price_track_header_tools.getViaIdOrContractStrict(header);
+            this.log(`retrieving price of pair ${eth_price_track_header_tools_1.eth_price_track_header_tools.getOrderedPairSymbol(header)} ${header.pair_contract}`, method);
+            const detail = await this.getDetail(header, time_or_log, strict);
+            if (detail.isNew())
+                throw new Error(`no price detail found`);
+            price = assert_1.assert.isNumeric(detail.price, `eth_price_track_detail.price(${detail.price}) is not numeric`);
+        }
+        catch (e) {
+            if (e instanceof Error) {
+                this.log(e.message, method, true, true);
+                if (strict)
+                    throw new eth_price_track_details_tools_error(e.message);
+            }
+        }
+        return price;
+    }
+    static async getBnbPrice(header, time_or_log, strict = false) {
+        const method = "getBnbPrice";
+        let bnb_price = "0.00";
+        try {
+            header = await eth_price_track_header_tools_1.eth_price_track_header_tools.getViaIdOrContractStrict(header);
+            this.log(`retrieving bnb price of pair ${eth_price_track_header_tools_1.eth_price_track_header_tools.getOrderedPairSymbol(header)} ${header.pair_contract}`, method);
+            const detail = await this.getDetail(header, time_or_log, strict);
+            if (detail.isNew())
+                throw new Error(`no price detail found`);
+            const price_bnb = assert_1.assert.stringNotEmpty(detail.price_bnb, "detail.price_bnb");
+            bnb_price = assert_1.assert.isNumeric(price_bnb, `eth_price_track_detail.price_bnb(${detail.price_bnb}) is not numeric`);
+        }
+        catch (e) {
+            if (e instanceof Error) {
+                this.log(e.message, method, true, true);
+                if (strict)
+                    throw new eth_price_track_details_tools_error(e.message);
+            }
+        }
+        return bnb_price;
+    }
+    static async getUsdPrice(header, time_or_log, strict = false) {
+        const method = "getUsdPrice";
+        let usd_price = "0.00";
+        try {
+            header = await eth_price_track_header_tools_1.eth_price_track_header_tools.getViaIdOrContractStrict(header);
+            this.log(`retrieving usd price of pair ${eth_price_track_header_tools_1.eth_price_track_header_tools.getOrderedPairSymbol(header)} ${header.pair_contract}`, method);
+            const detail = await this.getDetail(header, time_or_log, strict);
+            if (detail.isNew())
+                throw new Error(`no price detail found`);
+            const price_usd = assert_1.assert.stringNotEmpty(detail.price_usd, "detail.price_usd");
+            usd_price = assert_1.assert.isNumeric(price_usd, `eth_price_track_detail.price_usd(${detail.price_usd}) is not numeric`);
+        }
+        catch (e) {
+            if (e instanceof Error) {
+                this.log(e.message, method, true, true);
+                if (strict)
+                    throw new eth_price_track_details_tools_error(e.message);
+            }
+        }
+        return usd_price;
+    }
+    static async getBnbUsdPrice(time_or_log) {
+        return this.getPrice(eth_config_1.eth_config.getBnbUsdPairContract(), time_or_log);
+    }
+    static async getBnbUsdValue(time_or_log, token_amount) {
+        const bnbUsdPrice = await this.getBnbUsdPrice(time_or_log);
+        return tools_1.tools.toBn(bnbUsdPrice).multipliedBy(tools_1.tools.toBn(token_amount)).toFixed(eth_config_1.eth_config.getBusdDecimal());
+    }
+    static async getBnbTokenPrice(time_or_log, token) {
+        const method = "getBnbTokenPrice";
+        let price = "0.00";
+        this.log("retrieving bnb price of a token", method);
+        if (typeof token === "string")
+            token = await eth_contract_data_tools_1.eth_contract_data_tools.getContractViaAddressStrict(token);
+        this.log(`token symbol:${token.symbol}`, method);
+        const tokenBnbPair = await eth_price_track_header_tools_1.eth_price_track_header_tools.getViaTokenContracts(eth_config_1.eth_config.getEthContract(), token.address, false);
+        if (!tokenBnbPair) {
+            this.log(`BNB${token.symbol.toUpperCase()} pair does not exists`, method);
+        }
+        else {
+            this.log(`bnb pair found:${tokenBnbPair.pair_contract}`, method);
+            price = await this.getBnbPrice(tokenBnbPair, time_or_log, false);
+        }
+        return assert_1.assert.isNumeric(price);
+    }
+}
+exports.eth_price_track_details_tools = eth_price_track_details_tools;
+class eth_price_track_details_tools_error extends Error {
+    constructor(message) {
+        super(message);
+    }
+}
+//# sourceMappingURL=eth_price_track_details_tools.js.map
---
+++ eth_price_track_details_tools.ts
@@ -0,0 +1,222 @@
+import {eth_price_track_header} from "./build/eth_price_track_header";
+import {eth_price_track_details} from "./build/eth_price_track_details";
+import {eth_receipt_logs} from "./build/eth_receipt_logs";
+import { assert } from "./assert";
+import { config } from "./config";
+import { ContractInfo } from "./eth_types";
+import { eth_config } from "./eth_config";
+import { tools } from "./tools";
+import {eth_price_track_header_tools} from "./eth_price_track_header_tools";
+import {TIME_FORMATS, time_helper} from "./time_helper";
+import {eth_contract_data_tools} from "./eth_contract_data_tools";
+
+enum ORDER{
+    ASC="ASC",
+    DESC="DESC"
+}
+
+interface getDetailsArgument {
+    from_time?:number,
+    to_time?:number,
+    log?:eth_receipt_logs,
+    order?:ORDER,
+    limit?:number,
+}
+
+export class eth_price_track_details_tools{
+
+    private static log(msg:string,method:string,end:boolean=false,force_display:boolean=false):void{
+        if(config.getConfig().verbose_log || force_display){
+            console.log(`${this.name}|${method}|${msg}`);
+            if(end) console.log(tools.LINE);
+        }
+    }
+
+    public static async getDetails(
+        header:number|string|eth_price_track_header,
+        {from_time=0,to_time=0,log=undefined,order=ORDER.DESC,limit=1}:getDetailsArgument,
+        strict:boolean=false
+    ):Promise<eth_price_track_details|false>
+    {
+        const method = `getDetails`;
+        this.log(`retrieving price details. establishing header`,method);
+        const details = new eth_price_track_details();
+        try{
+            const retrievedHeader = await eth_price_track_header_tools.getViaIdOrContractStrict(header);
+            const orderString = ` ORDER BY blockNumber ${order}, logIndex ${order} LIMIT ${limit} `;
+            if(log){
+                this.log(`db log passed, retrieving via blockNumber(${log.blockNumber}) and logIndex(${log.logIndex})`,method);
+                await details.list(
+                    " WHERE header_id=:header_id AND blockNumber=:blockNumber AND logIndex<=:logIndex ",
+                    {header_id:retrievedHeader.id, blockNumber:assert.positiveNumber(log.blockNumber),logIndex:assert.naturalNumber(log.logIndex)},
+                    orderString);
+            }
+            if(details.count() === 0){
+                if(log){
+                    this.log(`no details found via logs method, retrieving via timestamp`,method);
+                }
+                let where = " WHERE header_id=:header_id ";
+                let param:{[key:string]:string|number} = {};
+                param["header_id"] = assert.positiveNumber(retrievedHeader.id);
+                if(from_time > 0){
+                    const fromTimeInfo = time_helper.getTime(from_time,"UTC");
+                    this.log(`from ${fromTimeInfo.format(TIME_FORMATS.READABLE)}`,method);
+                    where += " AND blockTime>=:from ";
+                    param["from"]=from_time;
+                }
+                else{
+                    this.log(`no from_time info`,method);
+                }
+                if(to_time > 0){
+                    const toTimeInfo = time_helper.getTime(to_time,"UTC");
+                    this.log(`to ${toTimeInfo.format(TIME_FORMATS.READABLE)}`,method);
+                    where += " AND blockTime<=:to ";
+                    param["to"] = to_time;
+                }
+                await details.list(where,param,orderString);
+            }
+        }catch (e){
+            if(e instanceof Error){
+                this.log(e.message,method,true,true);
+                if(strict) throw new eth_price_track_details_tools_error(e.message);
+            }
+            return false;
+        }
+        this.log(`found:${details.count()}`,method,true);
+        return details;
+    }
+    public static async getDetail(header:number|string|eth_price_track_header,time_or_log:number|string|eth_receipt_logs,strict:boolean=false):Promise<eth_price_track_details>{
+        const method = "getDetail";
+        let detail = new eth_price_track_details();
+        try{
+            header = await eth_price_track_header_tools.getViaIdOrContractStrict(header);
+            this.log(`retrieving price for ${eth_price_track_header_tools.getOrderedPairSymbol(header)}`,method);
+            const arg:{[key:string]:any} = {};
+            arg["order"] = ORDER.DESC;
+            arg["limit"] = 1;
+            if(typeof time_or_log === "number" || typeof time_or_log === "string"){
+                this.log(`time argument passed:${time_or_log}`,method);
+                const timeInfo = time_helper.getTime(time_or_log,"UTC");
+                arg["to_time"] = timeInfo.unix();
+            }
+            else{
+                this.log(`db log argument passed`,method);
+                arg["log"] = time_or_log;
+            }
+            const details = await this.getDetails(header,arg,strict);
+            if(!details) throw new Error(`no price details found`);
+            if(details.count() === 0)  throw new Error(`no price details found`);
+            detail = details.getItem();
+        }catch (e){
+            if(e instanceof Error){
+                this.log(e.message,method,true,true);
+                if(strict) throw new eth_price_track_details_tools_error(e.message);
+            }
+        }
+        return detail;
+    }
+    public static async getPrice(header:number|string|eth_price_track_header,time_or_log:number|string|eth_receipt_logs,strict:boolean=false):Promise<string>{
+        const method = "getPrice";
+        let price = "0.00";
+        try{
+            header = await eth_price_track_header_tools.getViaIdOrContractStrict(header);
+            this.log(`retrieving price of pair ${eth_price_track_header_tools.getOrderedPairSymbol(header)} ${header.pair_contract}`,method);
+            const detail = await this.getDetail(header,time_or_log,strict);
+            if(detail.isNew()) throw new Error(`no price detail found`);
+            price = assert.isNumeric<string>(detail.price,`eth_price_track_detail.price(${detail.price}) is not numeric`);
+        }catch (e){
+            if(e instanceof Error){
+                this.log(e.message,method,true,true);
+                if(strict) throw new eth_price_track_details_tools_error(e.message);
+            }
+        }
+        return price;
+    }
+    public static async getBnbPrice(header:number|string|eth_price_track_header,time_or_log:number|string|eth_receipt_logs,strict:boolean=false):Promise<string>{
+        const method = "getBnbPrice";
+        let bnb_price = "0.00";
+        try{
+            header = await eth_price_track_header_tools.getViaIdOrContractStrict(header);
+            this.log(`retrieving bnb price of pair ${eth_price_track_header_tools.getOrderedPairSymbol(header)} ${header.pair_contract}`,method);
+            const detail = await this.getDetail(header,time_or_log,strict);
+            if(detail.isNew()) throw new Error(`no price detail found`);
+            const price_bnb = assert.stringNotEmpty(detail.price_bnb,"detail.price_bnb");
+            bnb_price = assert.isNumeric<string>(price_bnb,`eth_price_track_detail.price_bnb(${detail.price_bnb}) is not numeric`);
+        }
+        catch (e) {
+            if(e instanceof Error){
+                this.log(e.message,method,true,true);
+                if(strict) throw new eth_price_track_details_tools_error(e.message);
+            }
+        }
+        return bnb_price;
+
+    }
+    public static async getUsdPrice(header:number|string|eth_price_track_header,time_or_log:number|string|eth_receipt_logs,strict:boolean=false):Promise<string>{
+        const method = "getUsdPrice";
+        let usd_price = "0.00";
+        try{
+            header = await eth_price_track_header_tools.getViaIdOrContractStrict(header);
+            this.log(`retrieving usd price of pair ${eth_price_track_header_tools.getOrderedPairSymbol(header)} ${header.pair_contract}`,method);
+            const detail = await this.getDetail(header,time_or_log,strict);
+            if(detail.isNew()) throw new Error(`no price detail found`);
+            const price_usd = assert.stringNotEmpty(detail.price_usd,"detail.price_usd");
+            usd_price = assert.isNumeric<string>(price_usd,`eth_price_track_detail.price_usd(${detail.price_usd}) is not numeric`);
+        }
+        catch (e){
+            if(e instanceof Error){
+                this.log(e.message,method,true,true);
+                if(strict) throw new eth_price_track_details_tools_error(e.message);
+            }
+        }
+        return usd_price;
+    }
+
+    public static async getBnbUsdPrice(time_or_log:number|string|eth_receipt_logs):Promise<string>{
+        return this.getPrice(eth_config.getBnbUsdPairContract(),time_or_log);
+    }
+    public static async getBnbUsdValue(time_or_log:number|string|eth_receipt_logs,token_amount:string):Promise<string>{
+        const bnbUsdPrice = await this.getBnbUsdPrice(time_or_log);
+        return tools.toBn(bnbUsdPrice).multipliedBy(tools.toBn(token_amount)).toFixed(eth_config.getBusdDecimal());
+    }
+
+    public static async getBnbTokenPrice(time_or_log:number|string|eth_receipt_logs,token:string|ContractInfo):Promise<string>{
+        const method = "getBnbTokenPrice";
+        let price = "0.00";
+        this.log("retrieving bnb price of a token",method);
+        if(typeof token === "string") token = await eth_contract_data_tools.getContractViaAddressStrict(token);
+
+
+        this.log(`token symbol:${token.symbol}`,method);
+        const tokenBnbPair = await eth_price_track_header_tools.getViaTokenContracts(eth_config.getEthContract(),token.address,false);
+        if(!tokenBnbPair){
+            this.log(`BNB${token.symbol.toUpperCase()} pair does not exists`,method);
+        }
+        else{
+            this.log(`bnb pair found:${tokenBnbPair.pair_contract}`,method);
+            price = await this.getBnbPrice(tokenBnbPair,time_or_log,false);
+        }
+        return assert.isNumeric<string>(price);
+    }
+    // public static async getBnbTokenValue(time_or_log:number|string|eth_receipt_logs,token:string|ContractInfo,token_amount:string):Promise<string>{
+    //     const method = "getBnbTokenValue";
+    //     let value = "0.00";
+    //     try{
+    //         if(typeof token === "string") token = await eth_contract_data_tools.getContractViaAddressStrict(token);
+    //         this.log(`retrieving bnb token value of ${token.symbol} with amount ${token_amount}`,method);
+    //         const bnb_price = await this.getBnbTokenPrice(time_or_log,token);
+    //         this.log(`bnb price of token is ${bnb_price}`,method);
+    //     }catch (e){
+    //
+    //     }
+    // }
+    //
+    // public static async getBnbMainTokenPrice(){}
+    // public static async getBnbMainTokenValue(){}
+}
+
+class eth_price_track_details_tools_error extends Error{
+    constructor(message:string) {
+        super(message);
+    }
+}
