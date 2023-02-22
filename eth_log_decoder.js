28dcad43929222cff94a88606421dbbfaf1188e9
adminailab <96946812+adminailab@users.noreply.github.com>
2023-02-22 13:33:41 +0800

    commit js map

--- eth_log_decoder.js
+++ eth_log_decoder.js
@@ -22,15 +22,6 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.eth_log_decoder = void 0;
 const eth_log_sig_1 = require("./build/eth_log_sig");
@@ -98,212 +89,197 @@ const MintLogCodec = t.type({
 }, "mint");
 //endregion
 class eth_log_decoder {
-    static decodeLog(log) {
-        return __awaiter(this, void 0, void 0, function* () {
-            if (typeof log.topics === "undefined")
-                throw new Error(`topics is not defined`);
-            if (log.topics.length === 0)
-                throw new Error(`there is no topics`);
-            // extract signature
-            let signature = log.topics[0];
-            signature = signature.replace(/^(0x)/, "");
-            // retrieve signature from DB
-            let logSig = new eth_log_sig_1.eth_log_sig();
-            logSig.signature = signature;
-            yield logSig.fetch();
-            if (logSig.isNew()) {
-                // console.warn(`signature not found on database for hash:${log.transactionHash} log_signature:${signature}`);
-                return { ContractInfo: {
-                        address: "",
-                        name: "",
-                        symbol: "",
-                        decimals: 0,
-                    }, method_name: "" };
-            }
-            ailab_core_1.assert.notEmpty(logSig.params_names, "params_names");
-            logSig.params_names = ailab_core_1.assert.isString({ val: logSig.params_names, prop_name: "params_names", strict: true });
-            // build data object
-            // For future reference example:
-            // -- Transfer(address indexed from,address indexed to,uint256 value);Transfer...
-            let method_object = {};
-            let parameters = logSig.params_names;
-            let parts = parameters.split(";");
-            parts = parts[0];
-            parts = parts.split("(");
-            const logMethodName = ailab_core_1.assert.isString({ val: parts[0], prop_name: "log method name", strict: true });
-            let args = ailab_core_1.assert.isString({ val: parts[1], prop_name: "log method arguments", strict: true });
-            args = args.replace(")", "");
-            //// get method name
-            method_object.method_name = logMethodName;
-            method_object.ContractInfo = {
-                address: "",
-                name: "",
-                symbol: "",
-                decimals: 0,
-            };
-            let contractMetaData = yield ailab_core_1.eth_worker.getContractMetaData(log.address);
-            ailab_core_1.assert.isset({ val: contractMetaData.symbol, prop_name: "contractMetaData.symbol", strict: true });
-            method_object.ContractInfo.address = log.address;
-            method_object.ContractInfo.name = contractMetaData.name;
-            method_object.ContractInfo.symbol = contractMetaData.symbol;
-            method_object.ContractInfo.decimals = contractMetaData.decimals;
-            //// segregate indexes and parameter name and values
-            //     parts = parts[1].replace(")","");
-            //     parts = parts.split(",");
-            const arg_parts = args.split(",");
-            let parameterNames = [];
-            let parameterTypes = [];
-            let topicLogIndex = 0;
-            for (let x = 0; x < arg_parts.length; x++) {
-                let parameter_parts = arg_parts[x].split(" ");
-                let isIndex = (typeof parameter_parts[1] === "string" && parameter_parts[1] === "indexed");
-                let parameter_name = parameter_parts[parameter_parts.length - 1];
-                let parameter_type = parameter_parts[0];
-                let parameter_value = "";
-                if (isIndex) {
-                    parameter_value = log.topics[++topicLogIndex].replace("0x000000000000000000000000", "0x");
-                    if (parameter_type.indexOf("uint") >= 0) {
-                        parameter_value = BigInt(parameter_value);
-                    }
-                }
-                else {
-                    parameterNames.push(parameter_name);
-                    parameterTypes.push(parameter_type);
-                }
-                method_object[parameter_name] = parameter_value;
-            }
-            //// decode data based on collected parameter types
-            let decodedDataCollection = Web3Client.eth.abi.decodeParameters(parameterTypes, log.data);
-            for (let x = 0; x < parameterNames.length; x++) {
-                if (typeof decodedDataCollection[x] === "undefined") {
-                    console.log("COLLECTED PARAMETERS");
-                    console.log(parameterNames);
-                    console.log("DECODED DATA COLLECTION");
-                    console.log(decodedDataCollection);
-                    throw new Error("decoded data collection do not match collected parameters");
-                }
-                if (typeof decodedDataCollection[x] === "string") {
-                    method_object[parameterNames[x]] = decodedDataCollection[x].replace("0x000000000000000000000000", "0x");
-                }
-                if (parameterTypes[x].indexOf("uint") >= 0) {
-                    method_object[parameterNames[x]] = BigInt(method_object[parameterNames[x]]);
+    static async decodeLog(log) {
+        if (typeof log.topics === "undefined")
+            throw new Error(`topics is not defined`);
+        if (log.topics.length === 0)
+            throw new Error(`there is no topics`);
+        // extract signature
+        let signature = log.topics[0];
+        signature = signature.replace(/^(0x)/, "");
+        // retrieve signature from DB
+        let logSig = new eth_log_sig_1.eth_log_sig();
+        logSig.signature = signature;
+        await logSig.fetch();
+        if (logSig.isNew()) {
+            // console.warn(`signature not found on database for hash:${log.transactionHash} log_signature:${signature}`);
+            return { ContractInfo: {
+                    address: "",
+                    name: "",
+                    symbol: "",
+                    decimals: 0,
+                }, method_name: "" };
+        }
+        ailab_core_1.assert.notEmpty(logSig.params_names, "params_names");
+        logSig.params_names = ailab_core_1.assert.isString({ val: logSig.params_names, prop_name: "params_names", strict: true });
+        // build data object
+        // For future reference example:
+        // -- Transfer(address indexed from,address indexed to,uint256 value);Transfer...
+        let method_object = {};
+        let parameters = logSig.params_names;
+        let parts = parameters.split(";");
+        parts = parts[0];
+        parts = parts.split("(");
+        const logMethodName = ailab_core_1.assert.isString({ val: parts[0], prop_name: "log method name", strict: true });
+        let args = ailab_core_1.assert.isString({ val: parts[1], prop_name: "log method arguments", strict: true });
+        args = args.replace(")", "");
+        //// get method name
+        method_object.method_name = logMethodName;
+        method_object.ContractInfo = {
+            address: "",
+            name: "",
+            symbol: "",
+            decimals: 0,
+        };
+        let contractMetaData = await ailab_core_1.eth_worker.getContractMetaData(log.address);
+        ailab_core_1.assert.isset({ val: contractMetaData.symbol, prop_name: "contractMetaData.symbol", strict: true });
+        method_object.ContractInfo.address = log.address;
+        method_object.ContractInfo.name = contractMetaData.name;
+        method_object.ContractInfo.symbol = contractMetaData.symbol;
+        method_object.ContractInfo.decimals = contractMetaData.decimals;
+        //// segregate indexes and parameter name and values
+        //     parts = parts[1].replace(")","");
+        //     parts = parts.split(",");
+        const arg_parts = args.split(",");
+        let parameterNames = [];
+        let parameterTypes = [];
+        let topicLogIndex = 0;
+        for (let x = 0; x < arg_parts.length; x++) {
+            let parameter_parts = arg_parts[x].split(" ");
+            let isIndex = (typeof parameter_parts[1] === "string" && parameter_parts[1] === "indexed");
+            let parameter_name = parameter_parts[parameter_parts.length - 1];
+            let parameter_type = parameter_parts[0];
+            let parameter_value = "";
+            if (isIndex) {
+                parameter_value = log.topics[++topicLogIndex].replace("0x000000000000000000000000", "0x");
+                if (parameter_type.indexOf("uint") >= 0) {
+                    parameter_value = BigInt(parameter_value);
                 }
             }
-            return method_object;
-        });
+            else {
+                parameterNames.push(parameter_name);
+                parameterTypes.push(parameter_type);
+            }
+            method_object[parameter_name] = parameter_value;
+        }
+        //// decode data based on collected parameter types
+        let decodedDataCollection = Web3Client.eth.abi.decodeParameters(parameterTypes, log.data);
+        for (let x = 0; x < parameterNames.length; x++) {
+            if (typeof decodedDataCollection[x] === "undefined") {
+                console.log("COLLECTED PARAMETERS");
+                console.log(parameterNames);
+                console.log("DECODED DATA COLLECTION");
+                console.log(decodedDataCollection);
+                throw new Error("decoded data collection do not match collected parameters");
+            }
+            if (typeof decodedDataCollection[x] === "string") {
+                method_object[parameterNames[x]] = decodedDataCollection[x].replace("0x000000000000000000000000", "0x");
+            }
+            if (parameterTypes[x].indexOf("uint") >= 0) {
+                method_object[parameterNames[x]] = BigInt(method_object[parameterNames[x]]);
+            }
+        }
+        return method_object;
     }
     //region Log Type Getters
-    static getTransferLog(log) {
-        return __awaiter(this, void 0, void 0, function* () {
-            let decodedLog = yield this.decodeLog(log);
-            if (!decodedLog)
-                return false;
-            if (decodedLog.method_name.toLowerCase() !== TransferLogCodec.name.toLowerCase())
-                return false;
-            let processedData = TransferLogCodec.decode(decodedLog);
-            if (d.isRight(processedData)) {
-                return processedData.right;
-            }
-            console.log(decodedLog);
-            console.log(processedData.left[0].context);
-            throw new Error("log is transfer but unable to identify type");
-        });
+    static async getTransferLog(log) {
+        let decodedLog = await this.decodeLog(log);
+        if (!decodedLog)
+            return false;
+        if (decodedLog.method_name.toLowerCase() !== TransferLogCodec.name.toLowerCase())
+            return false;
+        let processedData = TransferLogCodec.decode(decodedLog);
+        if (d.isRight(processedData)) {
+            return processedData.right;
+        }
+        console.log(decodedLog);
+        console.log(processedData.left[0].context);
+        throw new Error("log is transfer but unable to identify type");
     }
-    static getSwapLog(log) {
-        return __awaiter(this, void 0, void 0, function* () {
-            let decodedLog = yield this.decodeLog(log);
-            if (!decodedLog)
-                return false;
-            if (decodedLog.method_name.toLowerCase() !== SwapLogCodec.name)
-                return false;
-            let processedData = SwapLogCodec.decode(decodedLog);
-            if (d.isRight(processedData)) {
-                return processedData.right;
-            }
-            console.log(decodedLog);
-            console.log(processedData);
-            throw new Error("log is swap but unable to identify type");
-        });
+    static async getSwapLog(log) {
+        let decodedLog = await this.decodeLog(log);
+        if (!decodedLog)
+            return false;
+        if (decodedLog.method_name.toLowerCase() !== SwapLogCodec.name)
+            return false;
+        let processedData = SwapLogCodec.decode(decodedLog);
+        if (d.isRight(processedData)) {
+            return processedData.right;
+        }
+        console.log(decodedLog);
+        console.log(processedData);
+        throw new Error("log is swap but unable to identify type");
     }
-    static getApprovalLog(log) {
-        return __awaiter(this, void 0, void 0, function* () {
-            let decodedLog = yield this.decodeLog(log);
-            if (!decodedLog)
-                return false;
-            if (decodedLog.method_name.toLowerCase() !== ApprovalLogCodec.name)
-                return false;
-            let processedData = ApprovalLogCodec.decode(decodedLog);
-            if (d.isRight(processedData)) {
-                return processedData.right;
-            }
-            console.log(decodedLog);
-            console.log(processedData);
-            throw new Error("log is approval but unable to identify type");
-        });
+    static async getApprovalLog(log) {
+        let decodedLog = await this.decodeLog(log);
+        if (!decodedLog)
+            return false;
+        if (decodedLog.method_name.toLowerCase() !== ApprovalLogCodec.name)
+            return false;
+        let processedData = ApprovalLogCodec.decode(decodedLog);
+        if (d.isRight(processedData)) {
+            return processedData.right;
+        }
+        console.log(decodedLog);
+        console.log(processedData);
+        throw new Error("log is approval but unable to identify type");
     }
-    static getWithdrawalLog(log) {
-        return __awaiter(this, void 0, void 0, function* () {
-            let decodedLog = yield this.decodeLog(log);
-            if (!decodedLog)
-                return false;
-            if (decodedLog.method_name.toLowerCase() !== WithdrawalLogCodec.name)
-                return false;
-            let processedData = WithdrawalLogCodec.decode(decodedLog);
-            if (d.isRight(processedData)) {
-                return processedData.right;
-            }
-            console.log(decodedLog);
-            console.log(processedData);
-            throw new Error("log is withdrawal but unable to identify type");
-        });
+    static async getWithdrawalLog(log) {
+        let decodedLog = await this.decodeLog(log);
+        if (!decodedLog)
+            return false;
+        if (decodedLog.method_name.toLowerCase() !== WithdrawalLogCodec.name)
+            return false;
+        let processedData = WithdrawalLogCodec.decode(decodedLog);
+        if (d.isRight(processedData)) {
+            return processedData.right;
+        }
+        console.log(decodedLog);
+        console.log(processedData);
+        throw new Error("log is withdrawal but unable to identify type");
     }
-    static getSyncLog(log) {
-        return __awaiter(this, void 0, void 0, function* () {
-            let decodedLog = yield this.decodeLog(log);
-            if (!decodedLog)
-                return false;
-            if (decodedLog.method_name.toLowerCase() !== SyncLogCodec.name)
-                return false;
-            let processedData = SyncLogCodec.decode(decodedLog);
-            if (d.isRight(processedData)) {
-                return processedData.right;
-            }
-            console.log(decodedLog);
-            console.log(processedData);
-            throw new Error("log is sync but unable to identify type");
-        });
+    static async getSyncLog(log) {
+        let decodedLog = await this.decodeLog(log);
+        if (!decodedLog)
+            return false;
+        if (decodedLog.method_name.toLowerCase() !== SyncLogCodec.name)
+            return false;
+        let processedData = SyncLogCodec.decode(decodedLog);
+        if (d.isRight(processedData)) {
+            return processedData.right;
+        }
+        console.log(decodedLog);
+        console.log(processedData);
+        throw new Error("log is sync but unable to identify type");
     }
-    static getDepositLog(log) {
-        return __awaiter(this, void 0, void 0, function* () {
-            let decodedLog = yield this.decodeLog(log);
-            if (!decodedLog)
-                return false;
-            if (decodedLog.method_name.toLowerCase() !== DepositLogCodec.name)
-                return false;
-            let processedData = DepositLogCodec.decode(decodedLog);
-            if (d.isRight(processedData)) {
-                return processedData.right;
-            }
-            console.log(decodedLog);
-            console.log(processedData);
-            throw new Error("log is Deposit but unable to identify type");
-        });
+    static async getDepositLog(log) {
+        let decodedLog = await this.decodeLog(log);
+        if (!decodedLog)
+            return false;
+        if (decodedLog.method_name.toLowerCase() !== DepositLogCodec.name)
+            return false;
+        let processedData = DepositLogCodec.decode(decodedLog);
+        if (d.isRight(processedData)) {
+            return processedData.right;
+        }
+        console.log(decodedLog);
+        console.log(processedData);
+        throw new Error("log is Deposit but unable to identify type");
     }
-    static getMintLog(log) {
-        return __awaiter(this, void 0, void 0, function* () {
-            let decodedLog = yield this.decodeLog(log);
-            if (!decodedLog)
-                return false;
-            if (decodedLog.method_name.toLowerCase() !== MintLogCodec.name)
-                return false;
-            let processedData = MintLogCodec.decode(decodedLog);
-            if (d.isRight(processedData)) {
-                return processedData.right;
-            }
-            console.log(decodedLog);
-            console.log(processedData);
-            throw new Error("log is " + MintLogCodec.name + " but unable to identify type");
-        });
+    static async getMintLog(log) {
+        let decodedLog = await this.decodeLog(log);
+        if (!decodedLog)
+            return false;
+        if (decodedLog.method_name.toLowerCase() !== MintLogCodec.name)
+            return false;
+        let processedData = MintLogCodec.decode(decodedLog);
+        if (d.isRight(processedData)) {
+            return processedData.right;
+        }
+        console.log(decodedLog);
+        console.log(processedData);
+        throw new Error("log is " + MintLogCodec.name + " but unable to identify type");
     }
 }
 exports.eth_log_decoder = eth_log_decoder;
+//# sourceMappingURL=eth_log_decoder.js.map
