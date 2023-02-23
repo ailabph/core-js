dcdb6872cfa3dd3dd6ae58f03ea030d85b55d478
adminailab <96946812+adminailab@users.noreply.github.com>
2023-02-23 16:16:17 +0800

    placeholder class

---
+++ worker_events_trade_tools.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.worker_events_trade_tools = void 0;
+class worker_events_trade_tools {
+}
+exports.worker_events_trade_tools = worker_events_trade_tools;
+//# sourceMappingURL=worker_events_trade_tools.js.map
---
+++ worker_events_trade_tools.ts
@@ -0,0 +1,6 @@
+import {SwapLog} from "./web3_log_decoder";
+import {eth_receipt_logs_tools} from "./eth_receipt_logs_tools";
+
+export class worker_events_trade_tools{
+
+}
