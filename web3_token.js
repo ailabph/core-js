fbf17d6bdda35dff2c41ff81bc8805f4510cfaa8
adminailab <96946812+adminailab@users.noreply.github.com>
2023-02-23 16:15:28 +0800

    improved web3 token class

--- web3_token.js
+++ web3_token.js
@@ -5,66 +5,143 @@ const config_1 = require("./config");
 const eth_config_1 = require("./eth_config");
 const eth_rpc_1 = require("./eth_rpc");
 const tools_1 = require("./tools");
+const web3_rpc_web3_1 = require("./web3_rpc_web3");
 class web3_token {
     static log(msg, method, end = false, force_display = false) {
         if (config_1.config.getConfig().verbose_log || force_display) {
             console.log(`web3_token|${method}|${msg}`);
             if (end)
-                console.log(`${tools_1.tools.LINE}`);
+                console.log(`web3_token|${method}|${tools_1.tools.LINE}`);
         }
     }
     static initContract(address) {
         return eth_rpc_1.eth_rpc.getEtherContract(address, eth_config_1.eth_config.getTokenAbi());
     }
     //region READ
-    static async getName(contract_address, strict = false) {
-        const method = "getName";
-        this.log(`retrieving token name of ${contract_address}`, method);
+    static async genericFunctionCallNoArgs(contract_address, function_name, strict = false) {
+        const method = "genericFunctionCallNoArgs";
+        this.log(`calling function ${function_name} on contract ${contract_address}`, method);
         try {
-            const token_name = await this.initContract(contract_address).name();
-            this.log(`token name: ${token_name}`, method, true);
-            return token_name;
+            return await this.initContract(contract_address)[function_name]();
         }
         catch (e) {
-            if (e instanceof Error && !tools_1.tools.isEmpty(e.message)) {
-                this.log(e.message, method, true, true);
-                if (strict)
+            this.log(`ERROR: unable to call function ${function_name} from contract ${contract_address}`, `callFunctionWithNoArgument:${function_name}`, false, false);
+            if (strict) {
+                if (e instanceof Error)
                     throw new web3_token_error(e.message);
+                throw e;
             }
             return false;
         }
+    }
+    static async getName(contract_address, strict = false) {
+        const method = "getName";
+        this.log(`retrieving token name of ${contract_address}`, method);
+        return this.genericFunctionCallNoArgs(contract_address, "name", strict);
     }
     static async getSymbol(contract_address, strict = false) {
         const method = "getSymbol";
         this.log(`retrieving token symbol of ${contract_address}`, method);
-        try {
-            const tokenSymbol = await this.initContract(contract_address).symbol();
-            this.log(`token symbol: ${tokenSymbol}`, method, true);
-            return tokenSymbol;
-        }
-        catch (e) {
-            if (e instanceof Error && !tools_1.tools.isEmpty(e.message)) {
-                this.log(e.message, method, true, true);
-                if (strict)
-                    throw new web3_token_error(e.message);
-            }
-            return false;
-        }
+        return this.genericFunctionCallNoArgs(contract_address, "symbol");
     }
     static async getDecimals(contract_address, strict = false) {
         const method = "getDecimals";
         this.log(`retrieving token decimals of ${contract_address}`, method);
+        return this.genericFunctionCallNoArgs(contract_address, "decimals", strict);
+    }
+    static async getNameWeb3(contract_address, strict = false) {
+        const method = "getNameWeb3";
+        this.log(`retrieving name of contract ${contract_address}`, method);
+        const contract = web3_rpc_web3_1.web3_rpc_web3.getWeb3Contract(contract_address, [
+            {
+                "constant": true,
+                "inputs": [],
+                "name": "name",
+                "outputs": [
+                    {
+                        "internalType": "string",
+                        "name": "",
+                        "type": "string"
+                    }
+                ],
+                "payable": false,
+                "stateMutability": "view",
+                "type": "function"
+            }
+        ]);
         try {
-            const tokenDecimals = await this.initContract(contract_address).decimals();
-            this.log(`token decimals: ${tokenDecimals}`, method, true);
-            return tokenDecimals;
+            const name = await contract.methods.name().call();
+            this.log(`... name: ${name}`, method);
+            return name;
         }
         catch (e) {
-            if (e instanceof Error && !tools_1.tools.isEmpty(e.message)) {
-                this.log(e.message, method, true, true);
-                if (strict)
-                    throw new web3_token_error(e.message);
+            this.log(`ERROR, unable to retrieve name`, method);
+            if (strict)
+                throw e;
+            return false;
+        }
+    }
+    static async getSymbolWeb3(contract_address, strict = false) {
+        const method = "getSymbolWeb3";
+        this.log(`retrieving symbol of contract ${contract_address}`, method);
+        const contract = web3_rpc_web3_1.web3_rpc_web3.getWeb3Contract(contract_address, [
+            {
+                "constant": true,
+                "inputs": [],
+                "name": "symbol",
+                "outputs": [
+                    {
+                        "internalType": "string",
+                        "name": "",
+                        "type": "string"
+                    }
+                ],
+                "payable": false,
+                "stateMutability": "view",
+                "type": "function"
             }
+        ]);
+        try {
+            const symbol = await contract.methods.symbol().call();
+            this.log(`... symbol: ${symbol}`, method);
+            return symbol;
+        }
+        catch (e) {
+            this.log(`ERROR, unable to retrieve symbol`, method);
+            if (strict)
+                throw e;
+            return false;
+        }
+    }
+    static async getDecimalsWeb3(contract_address, strict = false) {
+        const method = "getDecimalsWeb3";
+        this.log(`retrieving decimals of contract ${contract_address}`, method);
+        const contract = web3_rpc_web3_1.web3_rpc_web3.getWeb3Contract(contract_address, [
+            {
+                "constant": true,
+                "inputs": [],
+                "name": "decimals",
+                "outputs": [
+                    {
+                        "internalType": "uint8",
+                        "name": "",
+                        "type": "uint8"
+                    }
+                ],
+                "payable": false,
+                "stateMutability": "view",
+                "type": "function"
+            }
+        ]);
+        try {
+            const decimals = await contract.methods.decimals().call();
+            this.log(`... decimals: ${decimals}`, method);
+            return decimals;
+        }
+        catch (e) {
+            this.log(`ERROR, unable to retrieve symbol`, method);
+            if (strict)
+                throw e;
             return false;
         }
     }
--- web3_token.ts
+++ web3_token.ts
@@ -3,13 +3,14 @@ import {eth_config} from "./eth_config";
 import {eth_rpc} from "./eth_rpc";
 import {tools} from "./tools";
 import {Contract} from "ethers";
+import {web3_rpc_web3} from "./web3_rpc_web3";
 
 export class web3_token{
 
     private static log(msg:string,method:string,end:boolean=false,force_display:boolean=false):void{
         if(config.getConfig().verbose_log || force_display){
             console.log(`web3_token|${method}|${msg}`);
-            if(end) console.log(`${tools.LINE}`);
+            if(end) console.log(`web3_token|${method}|${tools.LINE}`);
         }
     }
     private static initContract(address:string):Contract{
@@ -17,52 +18,136 @@ export class web3_token{
     }
 
     //region READ
-    public static async getName(contract_address:string,strict:boolean=false):Promise<string|false>{
-        const method = "getName";
-        this.log(`retrieving token name of ${contract_address}`,method);
+    public static async genericFunctionCallNoArgs<T>(contract_address:string, function_name:string, strict:boolean=false):Promise<T|false>{
+        const method = "genericFunctionCallNoArgs";
+        this.log(`calling function ${function_name} on contract ${contract_address}`,method);
+
         try{
-            const token_name = await this.initContract(contract_address).name();
-            this.log(`token name: ${token_name}`,method,true);
-            return token_name;
-        }catch (e){
-            if(e instanceof Error && !tools.isEmpty(e.message)) {
-                this.log(e.message,method,true,true);
-                if(strict) throw new web3_token_error(e.message);
+            return await this.initContract(contract_address)[function_name]() as T;
+        }catch (e) {
+            this.log(`ERROR: unable to call function ${function_name} from contract ${contract_address}`,`callFunctionWithNoArgument:${function_name}`,false,false);
+            if(strict){
+                if(e instanceof Error) throw new web3_token_error(e.message);
+                throw e;
             }
             return false;
         }
+    }
+    public static async getName(contract_address:string,strict:boolean=false):Promise<string|false>{
+        const method = "getName";
+        this.log(`retrieving token name of ${contract_address}`,method);
+        return this.genericFunctionCallNoArgs<string>(contract_address,"name",strict);
     }
     public static async getSymbol(contract_address:string,strict:boolean=false):Promise<string|false>{
         const method = "getSymbol";
         this.log(`retrieving token symbol of ${contract_address}`,method);
-        try{
-            const tokenSymbol = await this.initContract(contract_address).symbol();
-            this.log(`token symbol: ${tokenSymbol}`,method,true);
-            return tokenSymbol;
-        }catch (e){
-            if(e instanceof Error && !tools.isEmpty(e.message)) {
-                this.log(e.message,method,true,true);
-                if(strict) throw new web3_token_error(e.message);
-            }
-            return false;
-        }
+        return this.genericFunctionCallNoArgs<string>(contract_address,"symbol");
     }
     public static async getDecimals(contract_address:string,strict:boolean=false):Promise<bigint|false>{
         const method = "getDecimals";
         this.log(`retrieving token decimals of ${contract_address}`,method);
-        try{
-            const tokenDecimals = await this.initContract(contract_address).decimals();
-            this.log(`token decimals: ${tokenDecimals}`,method,true);
-            return tokenDecimals;
-        }catch (e){
-            if(e instanceof Error && !tools.isEmpty(e.message)) {
-                this.log(e.message,method,true,true);
-                if(strict) throw new web3_token_error(e.message);
+        return this.genericFunctionCallNoArgs<bigint>(contract_address,"decimals",strict);
+    }
+
+    public static async getNameWeb3(contract_address:string,strict:boolean=false):Promise<string|false>{
+        const method = "getNameWeb3";
+        this.log(`retrieving name of contract ${contract_address}`,method);
+        const contract = web3_rpc_web3.getWeb3Contract(contract_address,[
+            {
+                "constant": true,
+                "inputs": [],
+                "name": "name",
+                "outputs": [
+                    {
+                        "internalType": "string",
+                        "name": "",
+                        "type": "string"
+                    }
+                ],
+                "payable": false,
+                "stateMutability": "view",
+                "type": "function"
             }
+        ]);
+        try{
+            const name = await contract.methods.name().call();
+            this.log(`... name: ${name}`,method);
+            return name;
+        }
+        catch (e){
+            this.log(`ERROR, unable to retrieve name`,method);
+            if(strict) throw e;
+            return false;
+        }
+
+    }
+    public static async getSymbolWeb3(contract_address:string,strict:boolean=false):Promise<string|false>{
+        const method = "getSymbolWeb3";
+        this.log(`retrieving symbol of contract ${contract_address}`,method);
+        const contract = web3_rpc_web3.getWeb3Contract(contract_address,[
+            {
+                "constant": true,
+                "inputs": [],
+                "name": "symbol",
+                "outputs": [
+                    {
+                        "internalType": "string",
+                        "name": "",
+                        "type": "string"
+                    }
+                ],
+                "payable": false,
+                "stateMutability": "view",
+                "type": "function"
+            }
+        ]);
+        try{
+            const symbol = await contract.methods.symbol().call();
+            this.log(`... symbol: ${symbol}`,method);
+            return symbol;
+        }
+        catch (e){
+            this.log(`ERROR, unable to retrieve symbol`,method);
+            if(strict) throw e;
+            return false;
+        }
+    }
+    public static async getDecimalsWeb3(contract_address:string,strict:boolean=false):Promise<bigint|false>{
+        const method = "getDecimalsWeb3";
+        this.log(`retrieving decimals of contract ${contract_address}`,method);
+        const contract = web3_rpc_web3.getWeb3Contract(contract_address,[
+            {
+                "constant": true,
+                "inputs": [],
+                "name": "decimals",
+                "outputs": [
+                    {
+                        "internalType": "uint8",
+                        "name": "",
+                        "type": "uint8"
+                    }
+                ],
+                "payable": false,
+                "stateMutability": "view",
+                "type": "function"
+            }
+        ]);
+        try{
+            const decimals = await contract.methods.decimals().call();
+            this.log(`... decimals: ${decimals}`,method);
+            return decimals;
+        }
+        catch (e){
+            this.log(`ERROR, unable to retrieve symbol`,method);
+            if(strict) throw e;
             return false;
         }
     }
     //endregion READ
+
+    //region CHECKS
+
+    //endregion CHECKS
 }
 
 class web3_token_error extends Error{
