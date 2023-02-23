88f2a405b0d89519c31fdba44b9202fb2308631e
adminailab <96946812+adminailab@users.noreply.github.com>
2023-02-23 16:07:56 +0800

    remove web3 implementation

--- eth_rpc.ts
+++ eth_rpc.ts
@@ -6,18 +6,7 @@ export class eth_rpc {
     private static web3Provider:HttpProvider|undefined;
     private static web3Client:Web3|undefined;
 
-    public static getWeb3Provider():HttpProvider{
-        if(typeof eth_rpc.web3Provider !== "undefined") return eth_rpc.web3Provider;
-        eth_rpc.web3Provider = new Web3.providers.HttpProvider(eth_config.getRPCUrl());
-        return eth_rpc.web3Provider;
-    }
-
-    public static getWeb3Client():Web3{
-        if(typeof eth_rpc.web3Client !== "undefined") return eth_rpc.web3Client;
-        eth_rpc.web3Client = new Web3(eth_rpc.getWeb3Provider());
-        return eth_rpc.web3Client;
-    }
-
+    //region ETHER
     private static etherRpcProvider:JsonRpcProvider|undefined;
     public static getEtherProvider(){
         if(typeof this.etherRpcProvider !== "undefined") return this.etherRpcProvider;
@@ -27,4 +16,5 @@ export class eth_rpc {
     public static getEtherContract(contract_address:string, abi:any): ethers.Contract{
         return new ethers.Contract(contract_address, abi, this.getEtherProvider());
     }
+    //endregion ETHER
 }
