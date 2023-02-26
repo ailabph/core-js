fa4b8d5e541de51dd0262511fab39da62e3fb50d
adminailab <96946812+adminailab@users.noreply.github.com>
2023-02-26 23:15:19 +0800

    add is contract or wallet checker

--- web3_tools.js
+++ web3_tools.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.web3_tools = void 0;
 //region TYPE
+const web3_rpc_web3_1 = require("./web3_rpc_web3");
 var PAIR_POSITION;
 (function (PAIR_POSITION) {
     PAIR_POSITION[PAIR_POSITION["zero"] = 0] = "zero";
@@ -14,6 +15,13 @@ class web3_tools {
             return `${token0.toUpperCase()}${separator}${token1.toUpperCase()}`;
         else
             return `${token1.toUpperCase()}${separator}${token0.toUpperCase()}`;
+    }
+    static async isContractAddress(address) {
+        const code = await web3_rpc_web3_1.web3_rpc_web3.getWeb3Client().eth.getCode(address);
+        return code !== '0x' && code !== '0x0';
+    }
+    static async isWalletAddress(address) {
+        return web3_rpc_web3_1.web3_rpc_web3.getWeb3Client().utils.isAddress(address) && !(await this.isContractAddress(address));
     }
 }
 exports.web3_tools = web3_tools;
--- web3_tools.ts
+++ web3_tools.ts
@@ -1,5 +1,7 @@
 
 //region TYPE
+import {web3_rpc_web3} from "./web3_rpc_web3";
+
 enum PAIR_POSITION {
     zero,
     one,
@@ -11,6 +13,14 @@ export class web3_tools{
     public static getOrderedPair(token0:string,token1:string,position:PAIR_POSITION,separator:string=""):string{
         if(position === PAIR_POSITION.zero) return `${token0.toUpperCase()}${separator}${token1.toUpperCase()}`;
         else return `${token1.toUpperCase()}${separator}${token0.toUpperCase()}`;
+    }
+
+    public static async isContractAddress(address:string):Promise<boolean>{
+        const code = await web3_rpc_web3.getWeb3Client().eth.getCode(address);
+        return code !== '0x' && code !== '0x0';
+    }
+    public static async isWalletAddress(address:string):Promise<boolean>{
+        return web3_rpc_web3.getWeb3Client().utils.isAddress(address) && !(await this.isContractAddress(address));
     }
 
 }
