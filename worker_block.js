8fe41e77a7ada96086e048fcd7692c45bd918110
adminailab <96946812+adminailab@users.noreply.github.com>
2023-02-26 16:26:28 +0800

    initial draft worker block class

--- worker_block.js
+++ worker_block.js
@@ -1,7 +1,69 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const eth_worker_block_1 = require("./eth_worker_block");
-(async () => {
-    await eth_worker_block_1.eth_worker_block.run();
-})();
+exports.worker_block = void 0;
+const process_1 = require("process");
+const worker_events_token_1 = require("./worker_events_token");
+const connection_1 = require("./connection");
+const web3_rpc_web3_1 = require("./web3_rpc_web3");
+const tools_1 = require("./tools");
+const config_1 = require("./config");
+class worker_block {
+    static log(msg, method, end = false, force_display = false) {
+        if (config_1.config.getConfig().verbose_log || force_display) {
+            console.log(`worker_block|${method}|${msg}`);
+            if (end)
+                console.log(`worker_block|${method}|${tools_1.tools.LINE}`);
+        }
+    }
+    //region CONFIG
+    static getBatch() {
+        return 50;
+    }
+    //endregion CONFIG
+    static async run() {
+        /*
+        process:
+        blocks
+        from = get last block on db + 1
+        to = get latest block
+        to = to > limit ? limit : to;
+
+        loop from to
+            process++
+        single flight block
+        scan logs
+
+        if process is zero restart
+         */
+        await connection_1.connection.startTransaction();
+        try {
+            this.getBlockSingleFlight(123).then(() => {
+            });
+            await connection_1.connection.commit();
+        }
+        catch (e) {
+            await connection_1.connection.rollback();
+        }
+    }
+    static async getBlockSingleFlight(blockNumber) {
+        const method = "getBlockSingleFlight";
+        const blockNumberAsHex = tools_1.tools.convertNumberToHex(blockNumber);
+        this.log(`retrieving single flight block of ${blockNumber} as ${blockNumberAsHex}`, method);
+        return new Promise((resolve, reject) => {
+            web3_rpc_web3_1.web3_rpc_web3.getWeb3Provider().send({ jsonrpc: "2.0", method: "qn_getBlockWithReceipts", params: [blockNumberAsHex] }, (error, result) => {
+                if (error) {
+                    reject(error);
+                }
+                if (result) {
+                    resolve(result);
+                }
+            });
+        });
+    }
+}
+exports.worker_block = worker_block;
+if (process_1.argv.includes("run_worker_block")) {
+    console.log(`running worker to track token events`);
+    worker_events_token_1.worker_events_token.run().finally();
+}
 //# sourceMappingURL=worker_block.js.map
--- worker_block.ts
+++ worker_block.ts
@@ -1,5 +1,73 @@
-import {eth_worker_block} from "./eth_worker_block";
+import {argv} from "process";
+import {worker_events_token} from "./worker_events_token";
+import {connection} from "./connection";
+import {eth_rpc} from "./eth_rpc";
+import {web3_rpc_web3} from "./web3_rpc_web3";
+import {tools} from "./tools";
+import {config} from "./config";
 
-(async()=>{
-    await eth_worker_block.run();
-})();
+
+export class worker_block{
+
+    private static log(msg:string,method:string,end:boolean=false,force_display:boolean=false):void{
+        if(config.getConfig().verbose_log || force_display){
+            console.log(`worker_block|${method}|${msg}`);
+            if(end) console.log(`worker_block|${method}|${tools.LINE}`);
+        }
+    }
+
+    //region CONFIG
+    private static getBatch():number{
+        return 50;
+    }
+    //endregion CONFIG
+
+    public static async run(){
+        /*
+        process:
+        blocks
+        from = get last block on db + 1
+        to = get latest block
+        to = to > limit ? limit : to;
+
+        loop from to
+            process++
+        single flight block
+        scan logs
+
+        if process is zero restart
+         */
+
+        await connection.startTransaction();
+        try{
+            this.getBlockSingleFlight(123).then(()=>{
+
+            });
+            await connection.commit();
+        }catch (e){
+            await connection.rollback();
+        }
+    }
+
+    public static async getBlockSingleFlight(blockNumber:number):Promise<any>{
+        const method = "getBlockSingleFlight";
+        const blockNumberAsHex = tools.convertNumberToHex(blockNumber);
+        this.log(`retrieving single flight block of ${blockNumber} as ${blockNumberAsHex}`,method);
+        return new Promise((resolve, reject)=>{
+            web3_rpc_web3.getWeb3Provider().send({jsonrpc:"2.0",method:"qn_getBlockWithReceipts",params:[blockNumberAsHex]},(error,result)=>{
+                if(error){
+                    reject(error);
+                }
+                if(result){
+                    resolve(result);
+                }
+            });
+        });
+    }
+
+}
+
+if(argv.includes("run_worker_block")){
+    console.log(`running worker to track token events`);
+    worker_events_token.run().finally();
+}
