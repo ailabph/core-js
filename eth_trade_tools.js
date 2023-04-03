e030f47647163ef2dbff63337ad59c21aa0a0006
adminailab <96946812+adminailab@users.noreply.github.com>
2023-04-03 16:41:22 +0800

    add default eth_trade

--- eth_trade_tools.js
+++ eth_trade_tools.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.eth_trade_tools = exports.TRADE_STATUS = void 0;
 //region TYPES
+const eth_trade_1 = require("./build/eth_trade");
 var TRADE_STATUS;
 (function (TRADE_STATUS) {
     TRADE_STATUS["PENDING_OPEN"] = "pending_open";
@@ -14,6 +15,10 @@ exports.TRADE_STATUS = TRADE_STATUS;
 const eth_contract_events_1 = require("./build/eth_contract_events");
 const assert_1 = require("./assert");
 const eth_config_1 = require("./eth_config");
+const time_helper_1 = require("./time_helper");
+const eth_contract_data_tools_1 = require("./eth_contract_data_tools");
+const eth_price_track_header_tools_1 = require("./eth_price_track_header_tools");
+const tools_1 = require("./tools");
 class eth_trade_tools {
     //region GETTERS
     static async getTradeEvents(pairContract, from, to) {
@@ -23,6 +28,32 @@ class eth_trade_tools {
     }
     //endregion GETTERS
     //region UTILITIES
+    static async getDefault(base_contract, quote_contract) {
+        const method = "getDefault";
+        const baseInfo = await eth_contract_data_tools_1.eth_contract_data_tools.getContractViaAddress(base_contract);
+        if (!baseInfo)
+            throw new Error(`${method}|base_contract(${base_contract}) has no info on db or on chain`);
+        const quoteInfo = await eth_contract_data_tools_1.eth_contract_data_tools.getContractViaAddress(quote_contract);
+        if (!quoteInfo)
+            throw new Error(`${method}|quote_contract(${quote_contract}) has no info on db or on chain`);
+        const pairInfo = await eth_price_track_header_tools_1.eth_price_track_header_tools.getViaTokenContracts(base_contract, quote_contract);
+        if (!pairInfo)
+            throw new Error(`${method}|no pair info for base(${base_contract}) and quote(${quote_contract})`);
+        const newTrade = new eth_trade_1.eth_trade();
+        newTrade.pair = pairInfo.pair_contract;
+        newTrade.base_contract = base_contract;
+        newTrade.base_symbol = baseInfo.symbol;
+        newTrade.base_decimal = tools_1.tools.parseIntSimple(baseInfo.decimals, `baseInfo(${baseInfo.address}).decimals(${baseInfo.address})`);
+        newTrade.quote_contract = quote_contract;
+        newTrade.quote_symbol = baseInfo.symbol;
+        newTrade.quote_decimal = tools_1.tools.parseIntSimple(quoteInfo.decimals, `quoteInfo(${quoteInfo.address}).decimals(${quoteInfo.decimals})`);
+        newTrade.status = TRADE_STATUS.PENDING_OPEN;
+        newTrade.open_time_added = time_helper_1.time_helper.getCurrentTimeStamp();
+        newTrade.open_schedule = newTrade.open_time_added;
+        newTrade.open_expiry = newTrade.open_schedule + (60 * 3); // 3 minutes
+        newTrade.open_status = TRADE_STATUS.PENDING_OPEN;
+        return newTrade;
+    }
     static isBot(transaction) {
         const toAddress = assert_1.assert.stringNotEmpty(transaction.toAddress);
         return toAddress.toLowerCase() !== eth_config_1.eth_config.getDexContract().toLowerCase();
--- eth_trade_tools.ts
+++ eth_trade_tools.ts
@@ -1,5 +1,7 @@
 
 //region TYPES
+import {eth_trade} from "./build/eth_trade";
+
 enum TRADE_STATUS{
     PENDING_OPEN = "pending_open",
     OPEN = "open",
@@ -13,6 +15,13 @@ import {eth_contract_events} from "./build/eth_contract_events";
 import {eth_transaction} from "./build/eth_transaction";
 import {assert} from "./assert";
 import {eth_config} from "./eth_config";
+import {time_helper} from "./time_helper";
+import {web3_pair_price_tools} from "./web3_pair_price_tools";
+import {eth_worker} from "./eth_worker";
+import {web3_token} from "./web3_token";
+import {eth_contract_data_tools} from "./eth_contract_data_tools";
+import {eth_price_track_header_tools} from "./eth_price_track_header_tools";
+import {tools} from "./tools";
 
 export class eth_trade_tools{
     //region GETTERS
@@ -27,6 +36,30 @@ export class eth_trade_tools{
     //endregion GETTERS
 
     //region UTILITIES
+    public static async getDefault(base_contract:string, quote_contract:string):Promise<eth_trade>{
+        const method = "getDefault";
+        const baseInfo = await eth_contract_data_tools.getContractViaAddress(base_contract);
+        if(!baseInfo) throw new Error(`${method}|base_contract(${base_contract}) has no info on db or on chain`);
+        const quoteInfo = await eth_contract_data_tools.getContractViaAddress(quote_contract);
+        if(!quoteInfo) throw new Error(`${method}|quote_contract(${quote_contract}) has no info on db or on chain`);
+        const pairInfo = await eth_price_track_header_tools.getViaTokenContracts(base_contract,quote_contract);
+        if(!pairInfo) throw new Error(`${method}|no pair info for base(${base_contract}) and quote(${quote_contract})`);
+        
+        const newTrade = new eth_trade();
+        newTrade.pair = pairInfo.pair_contract;
+        newTrade.base_contract = base_contract;
+        newTrade.base_symbol = baseInfo.symbol;
+        newTrade.base_decimal = tools.parseIntSimple(baseInfo.decimals,`baseInfo(${baseInfo.address}).decimals(${baseInfo.address})`);
+        newTrade.quote_contract = quote_contract;
+        newTrade.quote_symbol = baseInfo.symbol;
+        newTrade.quote_decimal = tools.parseIntSimple(quoteInfo.decimals,`quoteInfo(${quoteInfo.address}).decimals(${quoteInfo.decimals})`);
+        newTrade.status = TRADE_STATUS.PENDING_OPEN;
+        newTrade.open_time_added = time_helper.getCurrentTimeStamp();
+        newTrade.open_schedule = newTrade.open_time_added;
+        newTrade.open_expiry = newTrade.open_schedule + (60 * 3); // 3 minutes
+        newTrade.open_status = TRADE_STATUS.PENDING_OPEN;
+        return newTrade;
+    }
     public static isBot(transaction:eth_transaction):boolean{
         const toAddress = assert.stringNotEmpty(transaction.toAddress);
         return toAddress.toLowerCase() !== eth_config.getDexContract().toLowerCase();
