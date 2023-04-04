69b2744a53c939cdb219c5666855f7b838e34826
adminailab <96946812+adminailab@users.noreply.github.com>
2023-04-04 13:19:26 +0800

    implement wallet_busd_balance

--- worker_request.js
+++ worker_request.js
@@ -3,19 +3,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.worker_request = void 0;
 const process_1 = require("process");
 const request_queue_1 = require("./build/request_queue");
-const connection_1 = require("./connection");
 const eth_worker_1 = require("./eth_worker");
 const tools_1 = require("./tools");
 const time_helper_1 = require("./time_helper");
+const web3_token_1 = require("./web3_token");
+const eth_config_1 = require("./eth_config");
 class worker_request {
     static async run() {
-        await connection_1.connection.startTransaction();
-        try {
-            let requests = new request_queue_1.request_queue();
-            await requests.list(" WHERE status!=:done ", { done: "d" }, ` ORDER BY id ASC LIMIT ${this.batchLimit} `);
-            if (requests.count() > 0)
-                console.log(`total request found:${requests.count()}`);
-            for (const request of requests._dataList) {
+        let requests = new request_queue_1.request_queue();
+        await requests.list(" WHERE status=:open ", { open: "o" }, ` ORDER BY id ASC LIMIT ${this.batchLimit} `);
+        if (requests.count() > 0)
+            console.log(`total request found:${requests.count()}`);
+        for (const request of requests._dataList) {
+            try {
                 if (typeof request.data_for !== "string")
                     throw new Error("data_for is empty");
                 console.log(`processing request ${request.hash} type ${request.type} for ${request.data_for} `);
@@ -25,22 +25,27 @@ class worker_request {
                 if (request.type === "wallet_eth_balance") {
                     request.data_result = await eth_worker_1.eth_worker.getETHBalance(request.data_for);
                 }
+                if (request.type === "wallet_busd_balance") {
+                    request.data_result = await web3_token_1.web3_token.getBalanceOf(eth_config_1.eth_config.getBusdContract(), request.data_for);
+                }
                 console.log(`--result:${request.data_result}`);
                 request.status = "d";
                 request.time_processed = time_helper_1.time_helper.getCurrentTimeStamp();
                 await request.save();
+                await tools_1.tools.sleep(150);
             }
-            await connection_1.connection.commit();
-            await tools_1.tools.sleep(150);
-            setImmediate(() => {
-                worker_request.run().finally();
-            });
+            catch (e) {
+                if (e instanceof Error) {
+                    console.log(`ERROR request(${request.id}) ${e.message}`);
+                }
+                request.status = "e";
+                request.time_processed = time_helper_1.time_helper.getCurrentTimeStamp();
+                await request.save();
+            }
         }
-        catch (e) {
-            await connection_1.connection.rollback();
-            console.log(`ERROR`);
-            console.log(e);
-        }
+        setImmediate(() => {
+            worker_request.run().finally();
+        });
     }
 }
 exports.worker_request = worker_request;
--- worker_request.ts
+++ worker_request.ts
@@ -6,20 +6,19 @@ import {connection} from "./connection";
 import {eth_worker} from "./eth_worker";
 import {tools} from "./tools";
 import {time_helper} from "./time_helper";
+import {web3_token} from "./web3_token";
+import {eth_config} from "./eth_config";
 
 export class worker_request{
 
     private static batchLimit:number = 10;
 
     public static async run(){
-
-        await connection.startTransaction();
-
-        try{
-            let requests = new request_queue();
-            await requests.list(" WHERE status!=:done ",{done:"d"},` ORDER BY id ASC LIMIT ${this.batchLimit} `);
-            if(requests.count()>0) console.log(`total request found:${requests.count()}`);
-            for(const request of requests._dataList as request_queue[]){
+        let requests = new request_queue();
+        await requests.list(" WHERE status=:open ",{open:"o"},` ORDER BY id ASC LIMIT ${this.batchLimit} `);
+        if(requests.count()>0) console.log(`total request found:${requests.count()}`);
+        for(const request of requests._dataList as request_queue[]){
+            try{
                 if (typeof request.data_for !== "string") throw new Error("data_for is empty");
                 console.log(`processing request ${request.hash} type ${request.type} for ${request.data_for} `);
                 if (request.type === "wallet_token_balance") {
@@ -28,21 +27,26 @@ export class worker_request{
                 if (request.type === "wallet_eth_balance") {
                     request.data_result = await eth_worker.getETHBalance(request.data_for);
                 }
+                if (request.type === "wallet_busd_balance") {
+                    request.data_result = await web3_token.getBalanceOf(eth_config.getBusdContract(),request.data_for);
+                }
                 console.log(`--result:${request.data_result}`);
                 request.status = "d";
                 request.time_processed = time_helper.getCurrentTimeStamp();
                 await request.save();
+                await tools.sleep(150);
+            }catch (e){
+                if(e instanceof Error){
+                    console.log(`ERROR request(${request.id}) ${e.message}`);
+                }
+                request.status = "e";
+                request.time_processed = time_helper.getCurrentTimeStamp();
+                await request.save();
             }
-            await connection.commit();
-            await tools.sleep(150);
-            setImmediate(()=>{
-                worker_request.run().finally();
-            });
-        }catch (e){
-            await connection.rollback();
-            console.log(`ERROR`);
-            console.log(e);
         }
+        setImmediate(()=>{
+            worker_request.run().finally();
+        });
     }
 
 }
