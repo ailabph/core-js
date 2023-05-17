418501fed554866c5766be86c6a29566d75e40d0
adminailab <96946812+adminailab@users.noreply.github.com>
2023-05-17 11:36:09 +0800

    process activation of trade and yacht club

--- worker_token_balance.js
+++ worker_token_balance.js
@@ -8,13 +8,18 @@ const config_1 = require("./config");
 const tools_1 = require("./tools");
 const time_helper_1 = require("./time_helper");
 const assert_1 = require("./assert");
+const eth_token_balance_1 = require("./build/eth_token_balance");
 const eth_receipt_logs_tools_1 = require("./eth_receipt_logs_tools");
 const eth_config_1 = require("./eth_config");
 const eth_token_balance_tools_1 = require("./eth_token_balance_tools");
 const eth_worker_1 = require("./eth_worker");
 const eth_price_track_details_tools_1 = require("./eth_price_track_details_tools");
 const web3_log_decoder_1 = require("./web3_log_decoder");
+const meta_options_1 = require("./build/meta_options");
 const meta_options_tools_1 = require("./meta_options_tools");
+const user_1 = require("./build/user");
+const eth_price_track_header_tools_1 = require("./eth_price_track_header_tools");
+const eth_price_track_details_1 = require("./build/eth_price_track_details");
 //endregion TYPES
 class worker_token_balance {
     static log(msg, method, end = false, force_display = false) {
@@ -219,6 +224,8 @@ class worker_token_balance {
         balanceDetail.activation_amount = balanceHeader.minimum_balance;
         await balanceDetail.save();
         await balanceHeader.save();
+        await this.processYachtClubActivation(balanceDetail);
+        await this.processTradeActivation(balanceDetail);
         return {
             balance_detail: balanceDetail,
             balance_header: balanceHeader,
@@ -229,6 +236,220 @@ class worker_token_balance {
     }
     static getMinimumBusdForActivation() {
         return 50;
+    }
+    static async processYachtClubActivation(balanceDetail) {
+        const method = "processYachtClubActivation";
+        assert_1.assert.inTransaction();
+        const balanceHeader = await eth_token_balance_tools_1.eth_token_balance_tools.getBalanceHeaderOf(balanceDetail.address);
+        const owner = new user_1.user();
+        owner.walletAddress = balanceDetail.address;
+        await owner.fetch();
+        if (owner.recordExists() && owner.usergroup === "yacht_club") {
+            this.log(`owner(${owner.username}) of wallet is yacht club member, processing activation info`, method, false, true);
+            if (tools_1.tools.isNullish(balanceHeader.activation_status_yacht_amount) || tools_1.tools.isEmpty(balanceHeader.activation_status_yacht_amount)) {
+                this.log(`...no yacht club activation info, initializing`, method, false, true);
+                const yacht_club_activation_usd = await this.getYachtClubActivationUsd();
+                this.log(`...activation usd value ${yacht_club_activation_usd}`, method, false, true);
+                if (!(owner.time_created > 0))
+                    throw new Error(`${method}|yacht club owner time_created(${owner.time_created}) invalid`);
+                const time_createdInfo = time_helper_1.time_helper.getTime(owner.time_created, "UTC");
+                this.log(`...user created on ${time_createdInfo.format(time_helper_1.TIME_FORMATS.READABLE)}`, method, false, true);
+                this.log(`...retrieving price`, method, false, true);
+                const tokenBnbPairHeader = await eth_price_track_header_tools_1.eth_price_track_header_tools.getViaIdOrContractStrict(eth_config_1.eth_config.getTokenBnbPairContract());
+                let price_string = await eth_price_track_details_tools_1.eth_price_track_details_tools.getUsdPrice(tokenBnbPairHeader, owner.time_created);
+                this.log(`...price ${price_string}`, method, false, true);
+                const price_number = tools_1.tools.parseNumber(price_string);
+                if (price_number <= 0) {
+                    this.log(`...no price, retrieving first price of pair`, method, false, true);
+                    const pairHeader = await eth_price_track_header_tools_1.eth_price_track_header_tools.getViaIdOrContractStrict(eth_config_1.eth_config.getTokenBnbPairContract());
+                    const details = new eth_price_track_details_1.eth_price_track_details();
+                    await details.list(" WHERE header_id=:header_id ", { header_id: pairHeader.id }, " ORDER BY blockNumber ASC, logIndex ASC LIMIT 1 ");
+                    if (details.count() == 0)
+                        throw new Error(`${method}| no price details found for token bnb pair`);
+                    const detail = details.getItem();
+                    price_string = assert_1.assert.isNumericString(detail.price_usd, `${method}|detail.price_usd`, 0);
+                    this.log(`...first price retrieved ${price_string}`, method, false, true);
+                }
+                assert_1.assert.positiveNumber(price_string, `${method}|price_string`);
+                const tokenAmount = tools_1.tools.divide(yacht_club_activation_usd, price_string, 16, `computing yacht activation token amount`);
+                this.log(`...token activation amount ${tokenAmount}`, method, false, true);
+                balanceHeader.activation_status_yacht_amount = tokenAmount;
+                await balanceHeader.save();
+            }
+            balanceHeader.activation_status_yacht_amount = assert_1.assert.stringNotEmpty(balanceHeader.activation_status_yacht_amount, `balanceHeader.activation_status_yacht_amount(${balanceHeader.activation_status_yacht_amount})`);
+            this.log(`...find any buy that is equal or more to ${balanceHeader.activation_status_yacht_amount}`, method, false, true);
+            const searchBuys = new eth_token_balance_1.eth_token_balance();
+            await searchBuys.list(" WHERE address=:address AND type=:buy", { address: balanceHeader.address, buy: "buy" }, " ORDER BY blockNumber ASC, logIndex ASC ");
+            this.log(`...puchased found ${searchBuys.count()}`, method, false, true);
+            let foundYachtActivation = false;
+            for (const log of searchBuys._dataList) {
+                if (!foundYachtActivation) {
+                    if (tools_1.tools.greaterThanOrEqualTo(log.debit, balanceHeader.activation_status_yacht_amount)) {
+                        foundYachtActivation = true;
+                        this.log(`...found purchase, hash ${log.transactionHash}`, method, false, true);
+                        if (log.activation_yacht_tag != "y") {
+                            log.activation_yacht_tag = "y";
+                            log.activation_status_yacht_amount = balanceHeader.activation_status_yacht_amount;
+                            log.activation_status_yacht = "y";
+                            await log.save();
+                        }
+                        break;
+                    }
+                }
+            }
+            // exclude old yacht club members from purchase check
+            if (!foundYachtActivation && owner.time_created >= time_helper_1.time_helper.getTime("2023-01-01", "Asia/Manila").startOf("D").unix()) {
+                this.log(`...this user has not purchased enough token to activate the yacht club`, method, false, true);
+                this.log(`...disabling yacht club status`, method, false, true);
+                balanceHeader.activation_status_yacht = "n";
+            }
+            else {
+                this.log(`...retrieving current token balance`, method, false, true);
+                const current_token_bal = await eth_worker_1.eth_worker.getTokenBalance(owner.walletAddress);
+                this.log(`...current token bal: ${current_token_bal}`, method, false, true);
+                this.log(`...min bal req: ${balanceHeader.activation_status_yacht_amount}`, method, false, true);
+                if (tools_1.tools.lesserThan(current_token_bal, balanceHeader.activation_status_yacht_amount)) {
+                    this.log(`...yacht user balance is below min, deactivating yacht status`, method, false, true);
+                    balanceHeader.activation_status_yacht = "n";
+                }
+                else {
+                    this.log(`...yacht user status is active`, method, false, true);
+                    balanceHeader.activation_status_yacht = "y";
+                }
+            }
+            await balanceHeader.save();
+            balanceDetail.activation_status_yacht = balanceHeader.activation_status_yacht;
+            balanceDetail.activation_status_yacht_amount = balanceHeader.activation_status_yacht_amount;
+            await balanceDetail.save();
+        }
+    }
+    static async getYachtClubActivationUsd() {
+        let yachtClubActivationValue = 500;
+        const yacht_club_activation = new meta_options_1.meta_options();
+        yacht_club_activation.tag = "yacht_club_activation";
+        await yacht_club_activation.fetch();
+        if (yacht_club_activation.recordExists()) {
+            yachtClubActivationValue = assert_1.assert.positiveNumber(yacht_club_activation.value, `yacht_club_activation.value`);
+        }
+        return yachtClubActivationValue;
+    }
+    static async processTradeActivation(balanceDetail) {
+        const method = "processTradeActivation";
+        assert_1.assert.inTransaction();
+        const balanceHeader = await eth_token_balance_tools_1.eth_token_balance_tools.getBalanceHeaderOf(balanceDetail.address);
+        const owner = new user_1.user();
+        owner.walletAddress = balanceDetail.address;
+        await owner.fetch();
+        if (owner.recordExists() && owner.usergroup === "yacht_club") {
+            // find token purchases after april 1, 2023
+            const validPurchaseDateFrom = time_helper_1.time_helper.getTime('2023-04-01', 'Asia/Manila').startOf("D").unix();
+            this.log(`...find any buy that is after april 1 2023`, method, false, true);
+            const searchBuys = new eth_token_balance_1.eth_token_balance();
+            await searchBuys.list(" WHERE address=:address AND type=:buy AND blockTime>=:from ", { address: balanceHeader.address, buy: "buy", from: validPurchaseDateFrom }, " ORDER BY blockNumber ASC, logIndex ASC ");
+            this.log(`...purchases found ${searchBuys.count()}`, method, false, true);
+            let foundTradeActivation = false;
+            let lastTradeActivationReq = "0.00";
+            for (const log of searchBuys._dataList) {
+                // if tagged already with yacht club activation, deduct it form consideration
+                this.log(`...check buy from block ${log.blockNumber} log ${log.logIndex} on ${time_helper_1.time_helper.getAsFormat(log.blockTime, time_helper_1.TIME_FORMATS.READABLE)} amount ${log.debit}`, method, false, true);
+                const tradeActivationUsd = this.getTradeActivationUsd(log.blockTime);
+                this.log(`...trade activation usd amount during this time is ${tradeActivationUsd}`, method, false, true);
+                this.log(`...retrieving price during this time`, method, false, true);
+                const tokenBnbHeader = await eth_price_track_header_tools_1.eth_price_track_header_tools.getViaIdOrContractStrict(eth_config_1.eth_config.getTokenBnbPairContract());
+                const price_string = await eth_price_track_details_tools_1.eth_price_track_details_tools.getUsdPrice(tokenBnbHeader, log.blockTime);
+                this.log(`...price of token during this time is ${price_string}`, method, false, true);
+                const price_number = assert_1.assert.positiveNumber(price_string, `${method}|price_string(${price_string})`);
+                if (price_number <= 0)
+                    throw new Error(`expected usd price for token on time ${time_helper_1.time_helper.getAsFormat(log.blockTime, time_helper_1.TIME_FORMATS.READABLE)}`);
+                const token_trade_activation_req = tools_1.tools.divide(tradeActivationUsd, price_string);
+                assert_1.assert.isNumericString(token_trade_activation_req, `${method}|token_trade_activation_req(${token_trade_activation_req})`, 0);
+                this.log(`...token equivalent for trade activation is ${token_trade_activation_req}`, method, false, true);
+                if (tradeActivationUsd > 0) {
+                    let token_bought = assert_1.assert.isNumericString(log.debit, `${method}|log.debit(${log.debit})`, 0);
+                    this.log(`...token_bought ${token_bought}`, method, false, true);
+                    if (log.activation_yacht_tag === "y") {
+                        this.log(`...transaction already used for yacht club activation, deducting activation_status_yacht_amount ${log.activation_status_yacht_amount}`, method, false, true);
+                        token_bought = tools_1.tools.subtract(token_bought, assert_1.assert.isNumericString(log.activation_status_yacht_amount));
+                        this.log(`...adjusted token_bought ${token_bought}`, method, false, true);
+                    }
+                    if (tools_1.tools.lesserThan(token_bought, 0)) {
+                        this.log(`...error, token_bought(${token_bought}) is less than 0`, method, false, true);
+                    }
+                    else {
+                        if (tools_1.tools.greaterThanOrEqualTo(token_bought, token_trade_activation_req)) {
+                            foundTradeActivation = true;
+                            this.log(`...trade activation detected for this transaction`, method, false, true);
+                            log.activation_status_trade = "y";
+                            log.activation_trade_tag = "y";
+                            log.activation_status_trade_amount = token_trade_activation_req;
+                            lastTradeActivationReq = token_trade_activation_req;
+                            await log.save();
+                            balanceHeader.activation_status_trade_hash = log.transactionHash;
+                            await balanceHeader.save();
+                            break;
+                        }
+                    }
+                }
+            }
+            if (foundTradeActivation) {
+                this.log(`...found valid trade purchase, proceeding with the next steps`, method, false, true);
+                this.log(`...check if still yacht club active`, method, false, true);
+                if (balanceDetail.activation_status_yacht === "y") {
+                    this.log(`...still yacht club active, proceeding`, method, false, true);
+                    this.log(`...retrieving current token balance`, method, false, true);
+                    const current_token_bal = await eth_worker_1.eth_worker.getTokenBalance(owner.walletAddress);
+                    if (tools_1.tools.greaterThanOrEqualTo(current_token_bal, lastTradeActivationReq)) {
+                        this.log(`...trade activated, updating`, method, false, true);
+                        balanceDetail.activation_status_trade_amount = lastTradeActivationReq;
+                        balanceDetail.activation_status_trade = "y";
+                        await balanceDetail.save();
+                        balanceHeader.activation_status_trade = "y";
+                        balanceHeader.activation_status_trade_amount = lastTradeActivationReq;
+                        await balanceHeader.save();
+                    }
+                    else {
+                        this.log(`...not enough balance, deactivating`, method, false, true);
+                        balanceDetail.activation_status_trade_amount = lastTradeActivationReq;
+                        balanceDetail.activation_status_trade = "n";
+                        await balanceDetail.save();
+                        balanceHeader.activation_status_trade = "n";
+                        balanceHeader.activation_status_trade_amount = lastTradeActivationReq;
+                        await balanceHeader.save();
+                    }
+                }
+                else {
+                    this.log(`...not yacht club active, deactivating`, method, false, true);
+                    balanceDetail.activation_status_trade_amount = lastTradeActivationReq;
+                    balanceDetail.activation_status_trade = "n";
+                    await balanceDetail.save();
+                    balanceHeader.activation_status_trade = "n";
+                    balanceHeader.activation_status_trade_amount = lastTradeActivationReq;
+                    await balanceHeader.save();
+                }
+            }
+            else {
+                this.log(`...no token purchased that is greater than ${lastTradeActivationReq}`, method, false, true);
+            }
+        }
+    }
+    static getTradeActivationUsd(time) {
+        const fromTimeFor100Usd = time_helper_1.time_helper.getTime('2023-04-01', 'Asia/Manila').startOf("D").unix();
+        const endTimeFor100Usd = time_helper_1.time_helper.getTime('2023-04-07', 'Asia/Manila').endOf("D").unix();
+        if (time >= fromTimeFor100Usd && time <= endTimeFor100Usd)
+            return 100;
+        const fromTimeFor150Usd = time_helper_1.time_helper.getTime('2023-04-08', 'Asia/Manila').startOf("D").unix();
+        const endTimeFor150Usd = time_helper_1.time_helper.getTime('2023-04-17', 'Asia/Manila').endOf("D").unix();
+        if (time >= fromTimeFor150Usd && time <= endTimeFor150Usd)
+            return 150;
+        const fromTimeFor200Usd = time_helper_1.time_helper.getTime('2023-04-18', 'Asia/Manila').startOf("D").unix();
+        const endTimeFor200Usd = time_helper_1.time_helper.getTime('2023-04-21', 'Asia/Manila').endOf("D").unix();
+        if (time >= fromTimeFor200Usd && time <= endTimeFor200Usd)
+            return 200;
+        const fromTimeFor250Usd = time_helper_1.time_helper.getTime('2023-04-22', 'Asia/Manila').startOf("D").unix();
+        if (time >= fromTimeFor250Usd)
+            return 250;
+        else
+            return 0;
     }
 }
 exports.worker_token_balance = worker_token_balance;
